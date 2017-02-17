cbe0acf932ef0eb0ba41545fb961bd11d040a6bd
tazboyz16 <tazboyz_16@yahoo.com>
2017-02-17 05:21:34 -0500

    Update FirstbootInstall.sh

--- myapps/FirstbootInstall.sh
+++ myapps/FirstbootInstall.sh
@@ -9,67 +9,67 @@ sudo apt-get install python2.7 mono-complete git-core python-lxml python-pip unz
 echo "<--- Running Install Scripts --->" 
 
 echo "<--- Installing iRedMail --->"
-bash /home/taz/install/mailinstaller.sh
+bash /home/xxxusernamexxx/install/mailinstaller.sh
 
 echo "<--- Installing Apache2 --->"
-bash /home/taz/install/Apache2-install.sh
+bash /home/xxxusernamexxx/install/Apache2-install.sh
 
 echo "<--- Installing Mysql and Phpmyadmin --->"
-bash /home/taz/install/Mysql.sh
+bash /home/xxxusernamexxx/install/Mysql.sh
 
 echo "<--- Installing Noip --->"
-bash /home/taz/install/Noip2Install.sh
+bash /home/xxxusernamexxx/install/Noip2Install.sh
 
 echo "<--- Installing Deluge --->"
-bash /home/taz/install/deluge_webui.sh
+bash /home/xxxusernamexxx/install/deluge_webui.sh
 
 echo "<--- Installing CouchPotato --->"
-bash /home/taz/install/couchpotato-installer.sh
+bash /home/xxxusernamexxx/install/couchpotato-installer.sh
 
 echo "<--- Installing Headphones --->"
-bash /home/taz/install/headphones-installer.sh
+bash /home/xxxusernamexxx/install/headphones-installer.sh
 
 echo "<--- Installing Mylar --->"
-bash /home/taz/install/mylar-installer.sh
+bash /home/xxxusernamexxx/install/mylar-installer.sh
 
 echo "<--- Installing SickRage --->"
-bash /home/taz/install/sickrage-installer.sh
+bash /home/xxxusernamexxx/install/sickrage-installer.sh
 
 echo "<--- Installing Webmin --->"
-bash /home/taz/install/webmin-installer.sh
+bash /home/xxxusernamexxx/install/webmin-installer.sh
 
 echo "<--- Installing Plex Media Server --->"
-bash /home/taz/install/plexinstall.sh
+bash /home/xxxusernamexxx/install/plexinstall.sh
 
 echo "<--- Installing Emby Media Server --->"
-bash /home/taz/install/EmbyServerInstall.sh
+bash /home/xxxusernamexxx/install/EmbyServerInstall.sh
 
 echo "<--- Installing Grive --->"
-bash /home/taz/install/GriveInstaller.sh
+bash /home/xxxusernamexxx/install/GriveInstaller.sh
 
 echo "<--- Installing ZoneMinder --->"
-bash /home/taz/install/zminstall.sh
+bash /home/xxxusernamexxx/install/zminstall.sh
 
 echo "<--- Installing TeamSpeak Server --->"
-bash /home/taz/install/ts3install.sh
+bash /home/xxxusernamexxx/install/ts3install.sh
 
 echo "<--- Installing Sinusbot --->"
-bash /home/taz/install/sinusbotinstall.sh
+bash /home/xxxusernamexxx/install/sinusbotinstall.sh
 
 echo "<--- Installing Samba --->"
-bash /home/taz/install/samba.sh
+bash /home/xxxusernamexxx/install/samba.sh
 
 echo "<--- Installing Mybb for Website Forums --->"
-bash /home/taz/install/Mybb.sh
+bash /home/xxxusernamexxx/install/Mybb.sh
 
 echo "<-- Installing Cron Jobs -->"
-bash /home/taz/install/Cronjobs.sh
+bash /home/xxxusernamexxx/install/Cronjobs.sh
 
 #echo "<--- Restoring Fstab settings --->"
-#cat /home/taz/install/configs/fstab.txt >> /etc/fstab
+#cat /home/xxxusernamexxx/install/configs/fstab.txt >> /etc/fstab
 
 echo "<---- Running Cleanup from Installs --->"
-bash /home/taz/Cleanup.sh
+bash /home/xxxusernamexxx/Cleanup.sh
 
 echo "<-----Reboot is needed to take effect of All System Restores and Installs------>"
 sleep 5
