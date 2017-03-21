fb94a5e92db41bee1fc0c26e78a8a61a990279bb
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-21 16:46:01 -0400

    Update PlexRestore.sh

--- myapps/beta/PlexRestore.sh
+++ myapps/beta/PlexRestore.sh
@@ -3,6 +3,6 @@
 sudo systemctl stop plexmediaserver
 tar xjf /media/SystemBackup/PlexBackup.tgz.bz2
 sudo chown xxxusernamexxx:xxxusernamexxx -R /var/lib/plexmediaserver/
-sudo cp /home/xxxusernamexxx/PlexBackup /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server
+sudo cp /home/xxxusernamexxx/PlexBackup /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Plugin\ Support/
 sleep 20
 sudo systemctl start plexmediaserver
