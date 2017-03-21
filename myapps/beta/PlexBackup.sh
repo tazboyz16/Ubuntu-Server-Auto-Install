dc28c125e801c0271702b1c7de6692c61308759d
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-21 16:45:17 -0400

    Update PlexBackup.sh

--- myapps/beta/PlexBackup.sh
+++ myapps/beta/PlexBackup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#/media/SystemBackup/ is my backup Flash Drive
-
 sudo systemctl stop plexmediaserver
-cd /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server
-sudo tar -cvf PlexBackup.tgz.bz2 --exclude='./Metadata' --exclude='./Cache' --exclude='./Plugin\ Support' . /media/SystemBackup/ 
+cd /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Plugin\ Support/
 
-#  "/media/SystemBackup/PlexBackup.tgz.bz2" 
-#   --exclude '/Plex\ Media\ Server/Cache/*' 
-#   --exclude '/Plex\ Media\ Server/Plugin\ Support/*' 
-#  /var/lib/plexmediaserver/Library/Application\ Support/
-
+sudo tar -cvf PlexBackup.tgz.bz2 * /media/SystemBackup/ 
 
 sudo systemctl start plexmediaserver
