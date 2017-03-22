292202e88dd4c98f8e18e5102e6a92ed3bd64e24
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-22 19:50:41 -0400

    Update forcegithubupdates.sh

--- myapps/update/forcegithubupdates.sh
+++ myapps/update/forcegithubupdates.sh
@@ -3,7 +3,7 @@
 echo "SickRage Auto Update"
 sudo systemctl stop sickrage
 sleep 5
-cd /home/xxxusernamexxx/.sickrage
+cd /opt/sickrage
 git fetch --all
 git reset --hard origin/master
 git pull
@@ -13,7 +13,7 @@ echo
 echo "Mylar Update"
 sudo systemctl stop Mylar
 sleep 5
-cd /home/xxxusernamexxx/.mylar
+cd /opt/mylar
 git fetch --all
 git reset --hard origin/master
 git pull
@@ -23,7 +23,7 @@ echo
 echo "Headphones Update"
 sudo systemctl stop Headphones
 sleep 5
-cd /home/xxxusernamexxx/.headphones
+cd /opt/headphones
 git fetch --all
 git reset --hard origin/master
 git pull
@@ -33,7 +33,7 @@ echo
 echo "CouchPotato Auto Update"
 sudo systemctl stop Couchpotato
 sleep 5
-cd /home/xxxusernamexxx/.couchpotato
+cd /opt/couchpotato
 git fetch --all
 git reset --hard origin/master
 git pull
