8116ce35ad37832e27ba1b0799b9f447f5d61ed0
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-23 10:49:02 -0400

    Add files via upload

---
+++ myapps/install/Apache2-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get install -yqq apache2
+sleep 15
+echo "<--- Restoring Apache2 Settings --->"
+cat /home/xxxusernamexxx/install/configs/apache2/apache2.conf > /etc/apache2/apache2.conf
+sleep 5
+echo "<- Restoring Apache2 Error Pages ->"
+cat /home/xxxusernamexxx/install/configs/apache2/localized-error-pages.conf > /etc/apache2/conf-available/localized-error-pages.conf
+systemctl restart apache2
+
+#echo "<--- Moving WWW files for Website --->"
+#rm -rf /var/www
+#tar xjf /home/xxxusernamexxx/install/www.tar.bz2
+#mv /home/xxxusernamexxx/www/ /var
+#echo
+#echo "<--- Changing Rights for dir WWW --->"
+#chmod 0777 -R /var/www
+#chown xxxusernamexxx:xxxusernamexxx -R /var/www
+
