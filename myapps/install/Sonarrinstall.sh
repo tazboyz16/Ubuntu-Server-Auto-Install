e957e346c17048b76c0c1e09153652b0ea06fe81
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-23 19:06:48 -0400

    Update Sonarrinstall.sh

--- myapps/install/Sonarrinstall.sh
+++ myapps/install/Sonarrinstall.sh
@@ -6,15 +6,15 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
 
 echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 
-sudo apt-get update && sudo apt-get install nzbdrone
+sudo apt-get update && sudo apt-get install nzbdrone libmono-cli-dev apt-transport-https
 
 sudo chown -R sonarr:sonarr /opt/NzbDrone
 
 #echo "<--- Restoring sonarr Settings --->"
 #cat /home/xxxusernamexxx/install/configs/programs/CouchPotato.txt > /opt/CouchPotatoServer/settings.conf
 
-#echo "Creating Startup Script"
-#cp /home/xxxusernamexxx/install/Services/sonarr.service /etc/systemd/system/
-#chmod 644 /etc/systemd/system/sonarr.service
-#systemctl enable sonarr.service
-#systemctl restart sonarr.service
+echo "Creating Startup Script"
+cp /home/xxxusernamexxx/install/Services/sonarr.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/sonarr.service
+systemctl enable sonarr.service
+systemctl restart sonarr.service
