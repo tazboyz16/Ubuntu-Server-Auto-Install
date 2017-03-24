13e6c72e173b81f0fbd77c1f64c32f1d35e0d2d1
tazboyz16 <tazboyz_16@yahoo.com>
2017-03-23 21:20:11 -0400

    Create radarrinstall.sh

---
+++ myapps/install/radarrinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt update && apt install libmono-cil-dev curl mediainfo
+
+sudo adduser --disabled-password --system --home /opt/ProgramData/radarr --gecos "Radarr Service" --group radarr
+
+echo "<--- Downloading latest Radarr --->"
+#At the time /v0.2.0.99 is the latest Radarr that is stable
+wget https://github.com/Radarr/Radarr/releases/download/v0.2.0.99/Radarr.develop.0.2.0.99.linux.tar.gz
+tar -xvzf Radarr.develop.0.2.0.99.linux.tar.gz
+
+
+#cd /opt && sudo git clone https://github.com/Radarr/Radarr/releases
+sudo chown -R radarr:radarr /opt/radarr/
+sudo chmod -R 0777 /opt/radarr
+
+
+echo "Creating Startup Script"
+cp /home/xxxusernamexxx/install/Services/radarr.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/radarr.service
+systemctl enable radarr.service
+systemctl restart radarr.service 
