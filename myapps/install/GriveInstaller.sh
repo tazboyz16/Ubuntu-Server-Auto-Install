7912f61f93a151c5a76eeab84d4fdec489dbcc32
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-12 16:46:57 -0400

    Delete GriveInstaller.sh

--- myapps/install/GriveInstaller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#http://www.webupd8.org/2015/05/grive2-grive-fork-with-google-drive.html has the DEB File ftp link
-
-echo "<--- Installing Grive --->"
-sudo add-apt-repository -y ppa:nilarimogard/webupd8
-echo "<-- Installing dependencies for Grive for Website Sync -->"
-sudo apt-get update
-sudo apt-get install -yqq grive
