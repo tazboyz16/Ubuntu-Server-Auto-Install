f8d3744c32210c785fa773a3ee0ad25ade18c4ee
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-12 23:48:38 -0400

    Delete samba.sh

--- myapps/install/samba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "<--- Setting up Samaba Smb Access Password --->"
-echo -e "xxxSambaPasswordxxx\nxxxSambaPasswordxxx" | sudo smbpasswd -a xxxusernamexxx
-echo "<--- Restoring Samba Settings --->"
-cat /home/xxxusernamexxx/install/configs/programs/Samba.txt > /etc/samba/smb.conf
-systemctl restart samba
