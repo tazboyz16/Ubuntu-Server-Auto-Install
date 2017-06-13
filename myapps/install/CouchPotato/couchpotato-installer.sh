6e504deff56ca2b69b873aafc8ca06e11932123c
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-13 19:54:10 -0400

    Create couchpotato-installer.sh

--- myapps/install/CouchPotato/couchpotato-installer.sh
+++ myapps/install/CouchPotato/couchpotato-installer.sh
@@ -15,9 +15,11 @@ sudo chmod -R 0777 /opt/ProgramData/
 #changing lines to edit SSL settings to accept the SSL Created in Certbot
 #line 5 and 7 for
 #ssl_key = and ssl_cert =
-#cp /home/xxxsernamexxx/install/CouchPotato/CouchPotato.txt /opt/ProgramData/couchpotato/.couchpotato/settings.conf
 sed -i "s/ssl_key = .*/ssl_key = /etc/apache2/ssl/apache.key" /opt/ProgramData/couchpotato/.couchpotato/settings.conf
 sed -i "s/ssl_cert = .*/ssl_cert = /etc/apache2/ssl/apache.crt/" /opt/ProgramData/couchpotato/.couchpotato/settings.conf
+
+#uncomment this line and comment out the sed lines if you have Couchpotato settings config saved
+#cp /home/xxxsernamexxx/install/CouchPotato/CouchPotato.txt /opt/ProgramData/couchpotato/.couchpotato/settings.conf
 
 echo "Creating Startup Script"
 cp /home/xxxusernamexxx/install/CouchPotato/couchpotato.service /etc/systemd/system/
