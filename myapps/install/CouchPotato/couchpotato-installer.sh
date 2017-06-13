9fdffccba3e79f5da7c2f87b042ede2fa6d895ce
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-13 19:35:55 -0400

    Create couchpotato-installer.sh

---
+++ myapps/install/CouchPotato/couchpotato-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get -yqq install git-core python python-cheetah python-pyasn1 python3-lxml
+
+sudo adduser --disabled-password --system --home /opt/ProgramData/couchpotato --gecos "CouchPotato Service" --group couchpotato
+
+echo "<--- Downloading latest CouchPotato --->"
+cd /opt && sudo git clone https://github.com/CouchPotato/CouchPotatoServer.git
+sudo chown -R couchpotato:couchpotato /opt/CouchPotatoServer/
+sudo chmod -R 0777 /opt/CouchPotatoServer
+
+echo "<--- Restoring CouchPotato Settings --->"
+sudo chmod -R 0777 /opt/ProgramData/
+
+#changing this line to edit SSL settings to accept the SSL Created in Certbot
+#cp /home/xxxsernamexxx/install/CouchPotato/CouchPotato.txt /opt/ProgramData/couchpotato/.couchpotato/settings.conf
+
+echo "Creating Startup Script"
+cp /home/xxxusernamexxx/install/CouchPotato/couchpotato.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/couchpotato.service
+systemctl enable couchpotato.service
+systemctl restart couchpotato.service 
