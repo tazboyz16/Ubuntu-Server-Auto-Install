f7cc1f41128991f1d8da37e664fc4d8e9202673e
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-13 20:49:27 -0400

    Create Apache2-install.sh

--- myapps/install/Apache2/Apache2-install.sh
+++ myapps/install/Apache2/Apache2-install.sh
@@ -9,11 +9,11 @@ echo "<- Restoring Apache2 Error Pages ->"
 cat /home/xxxusernamexxx/install/Apache2/localized-error-pages.conf > /etc/apache2/conf-available/localized-error-pages.conf
 systemctl restart apache2
 
-echo "<--- Moving WWW files for Website --->"
-rm -rf /var/www
-cd
-tar xjf /home/taz/install/Apache2/www.tar.bz2
-mv /home/xxxusernamexxx/www/ /var
+#echo "<--- Restoring WWW files for Website --->"
+#rm -rf /var/www
+#cd
+#tar xjf /home/taz/install/Apache2/www.tar.bz2
+#mv /home/xxxusernamexxx/www/ /var
 echo
 echo "<--- Changing Rights for dir WWW --->"
 chmod 0777 -R /var/www
