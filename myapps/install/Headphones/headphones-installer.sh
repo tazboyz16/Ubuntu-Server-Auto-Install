591f780d622197c98b26c12b8eca1347fc449443
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-13 20:20:48 -0400

    Create headphones-installer.sh

---
+++ myapps/install/Headphones/headphones-installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo adduser --disabled-password --system --home /opt/ProgramData/headphones --gecos "Headphones Service" --group headphones
+
+echo '<--- Downloading latest Headphones --->'
+cd /opt &&  git clone https://github.com/rembo10/headphones.git
+
+echo "<--- Restoring Headphones Settings --->"
+cat /home/xxxusernamexxx/install/Headphones/Headphones.txt > /opt/headphones/config.ini
+
+sudo chown -R headphones:headphones /opt/headphones
+sudo chmod -R 0777 /opt/headphones
+
+echo "Creating Startup Script"
+cp /home/xxxusernamexxx/install/Headphones/headphones.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/headphones.service
+systemctl enable headphones.service
+systemctl restart headphones.service
