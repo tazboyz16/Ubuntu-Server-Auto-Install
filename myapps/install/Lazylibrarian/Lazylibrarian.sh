e222e62d7258614fb84115f50f1fdf1b806a0995
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-14 22:30:20 -0400

    Create Lazylibrarian.sh

---
+++ myapps/install/Lazylibrarian/Lazylibrarian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo adduser --disabled-password --system --home /opt/ProgramData/LazyLibrarian --gecos "LazyLibrarian Service" --group LazyLibrarian
+
+cd /opt &&  git clone https://github.com/DobyTang/LazyLibrarian.git
+
+sudo chown -R LazyLibrarian:LazyLibrarian /opt/LazyLibrarian
+sudo chmod -R 0777 /opt/LazyLibrarian
+
+echo "Creating Startup Script" 
+cp /home/xxxusernamexxx/install/Lazylibrarian/LazyLibrarian.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/LazyLibrarian.service
+systemctl enable LazyLibrarian.service
+systemctl restart LazyLibrarian.service
