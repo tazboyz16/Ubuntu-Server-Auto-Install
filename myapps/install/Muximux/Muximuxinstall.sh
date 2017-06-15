db640f7c3f6ffd8de47bab612a0903ff500135c6
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-15 17:18:58 -0400

    Create Muximuxinstall.sh
    **beta**

---
+++ myapps/install/Muximux/Muximuxinstall.sh
@@ -0,0 +1,7 @@
+# /usr/bash
+
+sudo chmod 0777 -R /var/www
+git clone https://github.com/mescon/Muximux /var/www/html
+
+sudo chmod 0777 -R /var/www/html/Muximux
+sudo chown www-data:www-data -R /var/www/html/Muximux
