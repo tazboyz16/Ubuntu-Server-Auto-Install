901332ba2a3178b4b2ed43de1af73364e5d06581
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-15 17:16:49 -0400

    Create MadSonic.sh

---
+++ myapps/install/Madsonic/MadSonic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install openjdk-7-jre
+
+#Latest Stable as of 5/29/2017
+wget http://madsonic.org/download/6.2/20161208_madsonic-6.2.9040.deb
+sudo dpkg -i 20161208_madsonic-6.2.9040.deb
