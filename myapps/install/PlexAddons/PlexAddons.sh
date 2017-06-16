6cfbf16862691f060d73f18a00e5a2e9a467ebca
tazboyz16 <tazboyz_16@yahoo.com>
2017-06-15 21:43:12 -0400

    Create PlexAddons.sh

---
+++ myapps/install/PlexAddons/PlexAddons.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash /home/xxxusernamexxx/install/PlexAddons/plexpy.sh
+bash /home/xxxusernamexxx/install/PlexAddons/ombi.sh
+#installs webtools, sickrage, couchpotato, speedtest.net addon
+bash /home/xxxusernamexxx/install/PlexAddons/Webtools.sh
