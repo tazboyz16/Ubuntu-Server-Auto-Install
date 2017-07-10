4a05218dc1f2aa6c6bdf4ec3970af4ef52542635
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-09 23:50:10 -0400

    Create Port-Reconfig.sh

---
+++ myapps/beta/Port-Reconfig.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SCRIPTPATH=/opt/beta
+
+MAINCHOICE=$(whiptail --title "Port Reconfigure" \
+--menu "Which app would you like to manage?" --backtitle "$BACKTITLE" \
+--fb --cancel-button "Exit" $LINES $COLUMNS "$NETLINES" \
+"PVRs" "Automatic Media Library Manager" \
+"Usenet Downloaders" "Download binary files from Usenet servers" \
+"Bittorrent Clients" "Download Torrents" \
+"Personal Media Servers" "Organise and serve Media" \
+"Home Theater" "Media Playback" \
+"Sync Tools" "Keep files and folders synchronized" \
+"Administration Tools" "system configuration tools e.g. Webmin" \
+"Utilities" "Other miscellaneous tools" \
+"Maintenance" "Perform maintenance tasks" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [[ $exitstatus = 0 ]]; then
+    source "$SCRIPTPATH/inc/app-constant-reset.sh"
+    case "$MAINCHOICE" in
+        "PVRs" )
+            source "$SCRIPTPATH/menus/menu-pvrs.sh" ;;
+        "Usenet Downloaders" )
+            source "$SCRIPTPATH/menus/menu-usenet.sh" ;;
+        "Bittorrent Clients" )
+            source "$SCRIPTPATH/menus/menu-bittorrent.sh" ;;
+        "Personal Media Servers" )
+            source "$SCRIPTPATH/menus/menu-personal-media-servers.sh" ;;
+        "Home Theater" )
+            source "$SCRIPTPATH/menus/menu-home-theater.sh" ;;
+        "Sync Tools" )
+            source "$SCRIPTPATH/menus/menu-sync-tools.sh" ;;
+        "Administration Tools" )
+            source "$SCRIPTPATH/menus/menu-administration-tools.sh" ;;
+        "Utilities" )
+            source "$SCRIPTPATH/utils/utils-menu.sh" ;;
+        "Maintenance" )
+            source "$SCRIPTPATH/maintenance/maintenance-menu.sh" ;;
+        *)
+            echo -e "${RED}Invalid Option$ENDCOLOR"
+            source "$SCRIPTPATH/inc/exit.sh" ;;
+    esac
+else
+    echo
+    sleep 1
+    exit 0
+fi
