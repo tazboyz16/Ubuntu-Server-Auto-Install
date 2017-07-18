079816ee6fed2089523c8156446e2445d82a8058
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-18 13:40:01 -0400

    Update sinusbot.sh

--- myapps/install/SinusBot/sinusbot.sh
+++ myapps/install/SinusBot/sinusbot.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-sudo apt-get install -yqq debconf-utils lsb-release screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 less
+sudo apt install debconf-utils lsb-release screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 less python -y
 
-sudo adduser --disabled-password --system --home /opt/ProgramData/sinusbot --gecos "Sinusbot Service" --group sinusbot
+sudo adduser --disabled-password --system --home /opt/ProgramData/Sinusbot --gecos "Sinusbot Service" --group sinusbot
 
-mkdir -p /opt/sinusbot
-cd /opt/sinusbot
+mkdir -p /opt/Sinusbot
+cd /opt/Sinusbot
 wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
 
+#if first command fails
+#curl -O https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
+
 tar -xjf sinusbot-beta.tar.bz2
-
 #if new install
-cp config.ini.dist config.ini
-sed -i "s/TS3Path = .*/TS3Path = /opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64" /opt/sinusbot/config.ini
-sed -i "s/YoutubeDLPath = .*/YoutubeDLPath = /usr/local/bin/youtube-dl" /opt/sinusbot/config.ini
-sed -i "s/SSLKeyFile = .*/SSLKeyFile = /etc/apache2/ssl/apache.key" /opt/sinusbot/config.ini
-sed -i "s/SSLCertFile = .*/SSLCertFile = "/etc/apache2/ssl/apache.crt" /opt/sinusbot/config.ini
-
-
+#cp config.ini.dist config.ini
 #Saved settings
-#cp /home/xxxusernamexxx/install/SinusBot/config.ini /opt/sinusbot/config.ini
+cp /opt/install/SinusBot/config.ini /opt/Sinusbot/config.ini
 
-chown -R sinusbot:sinusbot /opt/sinusbot
-chmod 0755 -R /opt/sinusbot
+chown -R sinusbot:sinusbot /opt/Sinusbot
+chmod 0777 -R /opt/Sinusbot
 
 wget http://dl.4players.de/ts/releases/3.0.18.2/TeamSpeak3-Client-linux_amd64-3.0.18.2.run
 chmod 0755 TeamSpeak3-Client-linux_amd64-3.0.18.2.run
-./TeamSpeak3-Client-linux_amd64-3.0.18.2.run
+printf '\ny\n' | LESS='+q' ./TeamSpeak3-Client-linux_amd64-3.0.18.2.run
 
-cp -p -R /home/xxxusernamexxx/install/SinusBot/youtube-dl /usr/local/bin/
+curl -I http://yt-dl.org/downloads/latest/youtube-dl -o /opt/Sinusbot/youtube-dl
+chmod a+rx /opt/Sinusbot/youtube-dl
+
 cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
-cd
-chown -R sinusbot:sinusbot /opt/sinusbot
-chmod 0755 -R /opt/sinusbot
 
-./sinusbot --pwreset=xxxpasswordxxx
+chown -R sinusbot:sinusbot /opt/Sinusbot
+chmod 0777 -R /opt/Sinusbot
 
 echo "Creating Sinusbot startup Script"
-cp /home/xxxusernamexxx/install/SinusBot/sinusbot.service /etc/systemd/system/
+cp /opt/install/SinusBot/sinusbot.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/sinusbot.service
 systemctl enable sinusbot.service
 systemctl restart sinusbot.service
+
+cd
+echo "Backing up default Admin Password to /opt/Sinusbot"
+#su sinusbot -c "screen -A -m -d -S sinusbot /opt/Sinusbot/sinusbot -pwreset=foobar"
