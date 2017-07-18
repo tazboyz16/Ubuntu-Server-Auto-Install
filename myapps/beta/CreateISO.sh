42d6733e9389b4b8f1a8d62027a09f7bec78fcc6
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-17 23:33:30 -0400

    Update CreateISO.sh
    Trying to Create a Create ISO script to fully setup the myapss without searching throu the Configs, and installer scripts

--- myapps/beta/CreateISO.sh
+++ myapps/beta/CreateISO.sh
@@ -10,4 +10,19 @@
 #http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-amd64.iso
 #http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-i386.iso
 
-echo "Fully automated Script to Download Your Ubuntu ISO, Unpack it, edit the MyApps Scripts and then ReImage the ISO back together for you"
+echo "Fully Automated Script to Download Your Ubuntu ISO, "
+echo "Unpack it, edit the MyApps Scripts and then ReImage the ISO back together for you"
+
+echo "Where is the Myapps Folder located at?
+echo "If located in Your Home Dir please add the Full location with /home/'username' "
+read WorkingDir
+
+rm $WorkingDir/README.md
+rm $WorkingDir/_config.yml
+
+echo "Admin Account UserName?"
+read AdminUsername
+
+echo "Admin Account Password?"
+read AdminPassword
+
