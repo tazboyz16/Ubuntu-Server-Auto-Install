6815ed4f457a56f3cccafa2d9f00a1819317c761
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-17 23:25:33 -0400

    Create CreateISO.sh

---
+++ myapps/beta/CreateISO.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Example Locations of Ubuntu ISOs
+#http://releases.ubuntu.com/16.04/
+#http://releases.ubuntu.com/16.04.2/
+#http://releases.ubuntu.com/16.10/
+#http://releases.ubuntu.com/17.04/
+#http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-amd64.iso
+#http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-i386.iso
+#http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-amd64.iso
+#http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-i386.iso
+
+echo "Fully automated Script to Download Your Ubuntu ISO, Unpack it, edit the MyApps Scripts and then ReImage the ISO back together for you"
