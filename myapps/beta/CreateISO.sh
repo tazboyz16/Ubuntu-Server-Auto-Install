7afe70fb7857c02cab99aa3b33ea2dbeb08f5538
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-19 18:18:07 -0400

    Update CreateISO.sh

--- myapps/beta/CreateISO.sh
+++ myapps/beta/CreateISO.sh
@@ -17,7 +17,7 @@
 echo "Fully Automated Script to Download Your Ubuntu ISO, "
 echo "Unpack it, edit the MyApps Scripts and then ReImage the ISO back together for you"
 
-echo "Where is the Myapps Folder located at?'
+echo "Where is the Myapps Folder located at?"
 echo "If located in Your Home Dir please add the Full location with /home/'username' "
 read WorkingDir
 
