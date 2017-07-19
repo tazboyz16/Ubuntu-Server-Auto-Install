667715e8819a063a891840269963319a64d1db83
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-19 14:59:27 -0400

    Update FirstbootInstall.sh

--- myapps/FirstbootInstall.sh
+++ myapps/FirstbootInstall.sh
@@ -4,106 +4,108 @@ echo "<---Updaing System....Please Wait..--->"
 sudo apt-get update -qq
 sudo apt-get upgrade -yqq
 
-sudo apt-get install -yqq python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 python python-cheetah python-pyasn1 unrar-free openssl libssl-dev
+sudo apt install python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 python python-cheetah python-pyasn1 unrar-free openssl libssl-dev -y
 
 echo "<--- Running Install Scripts --->" 
 
 #Web Programs
 echo "<--- Installing iRedMail --->"
-bash /home/xxxusernamexxx/install/Iredmail/mailinstaller.sh
+bash /opt/install/Iredmail/mailinstaller.sh
 
 echo "<--- Installing Apache2 --->"
-bash /home/xxxusernamexxx/install/Apache2/Apache2-install.sh
+bash /opt/install/Apache2/Apache2-install.sh
 
 echo "<--- Installing SSL Cert --->"
-bash /home/xxxusernamexxx/install/Apache2/Certbot.sh
+bash /opt/install/Apache2/Certbot.sh
 
 echo "<--- Installing Mysql and Phpmyadmin --->"
-bash /home/xxxusernamexxx/install/Apache2/Mysql.sh
+bash /opt/install/Apache2/Mysql.sh
 
 #DDNS
 echo "<--- Installing Noip --->"
-bash /home/xxxusernamexxx/install/Noip2/Noip2Install.sh
+bash /opt/install/Noip2/Noip2Install.sh
 
 #
 echo "<--- Installing Deluge --->"
-bash /home/xxxusernamexxx/install/Deluge/deluge_webui.sh
+bash /opt/install/Deluge/deluge_webui.sh
 
 echo "<--- Installing CouchPotato --->"
-bash /home/xxxusernamexxx/install/couchpotato-installer.sh
+bash /opt/install/couchpotato-installer.sh
 
 echo "<--- Installing Headphones --->"
-bash /home/xxxusernamexxx/install/Headphones/headphones-installer.sh
+bash /opt/install/Headphones/headphones-installer.sh
 
 echo "<--- Installing Mylar --->"
-bash /home/xxxusernamexxx/install/Mylar/mylar-installer.sh
+bash /opt/install/Mylar/mylar-installer.sh
 
 echo "<--- Installing SickRage --->"
-bash /home/xxxusernamexxx/install/SickRage/sickrage-installer.sh
+bash /opt/install/SickRage/sickrage-installer.sh
 
 echo "<--- Installing Webmin --->"
-bash /home/xxxusernamexxx/install/Webmin/webmin-installer.sh
+bash /opt/install/Webmin/webmin-installer.sh
 
 echo "<--- Installing Plex Media Server --->"
-bash /home/xxxusernamexxx/install/Plex/plexinstall.sh
+bash /opt/update/plexupdate.sh -p -a -d
+
+echo "Installing 3rd Party Plex Addons"
+bash /opt/install/PlexAddons/PlexAddons.sh
 
 echo "<--- Installing Emby Media Server --->"
-bash /home/xxxusernamexxx/install/EmbyServer/EmbyServerInstall.sh
+bash /opt/install/EmbyServer/EmbyServerInstall.sh
 
 echo "<--- Installing Grive --->"
-bash /home/xxxusernamexxx/install/Grive/GriveInstaller.sh
+bash /opt/install/Grive/GriveInstaller.sh
 
 echo "<--- Installing ZoneMinder --->"
-bash /home/xxxusernamexxx/install/Zoneminder/zminstall.sh
+bash /opt/install/Zoneminder/zminstall.sh
 
 echo "<--- Installing TeamSpeak Server --->"
-bash /home/xxxusernamexxx/install/TeamSpeak3/ts3install.sh
+bash /opt/install/TeamSpeak3/ts3install.sh
 
 echo "Installing Sonarr"
-bash /home/xxxusernamexxx/install/Sonarr/sonarrinstall.sh
+bash /opt/install/Sonarr/sonarrinstall.sh
 
 echo "installing Jackett"
-bash /home/xxxusernamexxx/install/Jackett/jackettinstall.sh
+bash /opt/install/Jackett/jackettinstall.sh
 
 echo "<--- Installing Samba --->"
-bash /home/xxxusernamexxx/install/Samba/samba.sh
+bash /opt/install/Samba/samba.sh
 
 echo "<--- Installing Kodi --->"
-bash /home/xxxusernamexxx/install/Kodi/Kodi-install.sh
+bash /opt/install/Kodi/Kodi-install.sh
 
 echo "<--- Installing Muximux --->"
-bash /home/xxxusernamexxx/install/Muximux/Muximuxinstall.sh
+bash /opt/install/Muximux/Muximuxinstall.sh
 
 echo "<--- Installing HTPC-Manager --->"
-bash /home/xxxusernamexxx/HTPCManager/HTPCManager.sh
+bash /opt/HTPCManager/HTPCManager.sh
 
 echo "<--- Installing LazyLibrarian --->"
-bash /home/xxxusernamexxx/Lazylibrarian/Lazylibrarian.sh
+bash /opt/Lazylibrarian/Lazylibrarian.sh
 
 echo "<--- Installing Shinobi --->"
-bash /home/xxxusernamexxx/Shinobi/Shinobi.sh
+bash /opt/Shinobi/Shinobi.sh
 
 echo "<--- Installing MadSonic --->"
-bash /home/xxxusernamexxx/Madsonic/MadSonic.sh
+bash /opt/Madsonic/MadSonic.sh
 
 echo "<--- Installing Organizr --->"
-bash /home/xxxusernamexxx/Organizr/Organizr.sh
+bash /opt/Organizr/Organizr.sh
 
 echo "<--- Installing Ubooquity --->"
-bash /home/xxxusernamexxx/Organizr/Ubooquity.sh
+bash /opt/Organizr/Ubooquity.sh
 
-#commented out due to TS3 client requires interaction for install
-#echo "<--- Installing SinusBot --->"
-#bash /home/xxxusernamexxx/install/SinusBot/sinusbot.sh
+echo "<--- Installing SinusBot --->"
+bash /opt/install/SinusBot/sinusbot.sh
 
 echo "<-- Installing Cron Jobs -->"
-bash /home/xxxusernamexxx/install/System/Cronjobs.sh
+bash /opt/install/System/Cronjobs.sh
 
 #echo "<--- Restoring Fstab settings --->"
-#cat /home/xxxusernamexxx/install/System/fstab.txt >> /etc/fstab
+#cat /opt/install/System/fstab.txt >> /etc/fstab
 
 echo "<---- Running Cleanup from Installs --->"
-bash /home/xxxusernamexxx/Cleanup.sh
+bash /opt/Cleanup.sh
 
 echo "<-----Reboot is needed to take effect of All System Restores and Installs------>"
 sleep 5
