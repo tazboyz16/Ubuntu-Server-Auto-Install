29270a32e663df41f795e9c4238a8f43c4452f94
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-19 14:57:40 -0400

    Update Cleanup.sh

--- myapps/Cleanup.sh
+++ myapps/Cleanup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "<---- Cleaning Up Install files --->"
-rm -r /home/xxxusernamexxx/WebTools.bundle/
-rm -r /home/xxxusernamexxx/install/
-rm -r /home/xxxusernamexxx/ts3/
-rm -r /home/xxxusernamexxx/iRedMail-*
-rm -r /home/xxxusernamexxx/sinusbot/
+rm -r /opt/WebTools.bundle/
+rm -r /opt/install/
+rm -r /opt/ts3/
+rm -r /opt/iRedMail-*
+rm -r /opt/sinusbot/
+rm -r /opt/Jackett
 
 sudo chmod 0777 -R /opt/
 sudo apt-get update -qq
 sudo apt-get upgrade -yqq
-
 
 systemctl disable Installserver.service
 systemctl mask Installserver.service
