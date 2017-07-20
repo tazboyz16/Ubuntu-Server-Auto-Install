833faca2762d3af6b97352af0bef0d1097c72165
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 01:59:51 -0400

    Update sickrage-installer.sh

--- myapps/install/Sickrage/sickrage-installer.sh
+++ myapps/install/Sickrage/sickrage-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '<--- Installing prerequisites for SickRage --->'
-sudo apt-get -yqq install git-core unrar-free openssl libssl-dev python2.7
+sudo apt install git-core unrar-free openssl libssl-dev python2.7 -y
 
 sudo adduser --disabled-password --system --home /opt/ProgramData/sickrage --gecos "Sickrage Service" --group sickrage
 
@@ -11,14 +11,15 @@ cd /opt && sudo git clone https://github.com/SickRage/SickRage.git
 echo "<--- Restoring SickRage Settings --->"
 #backup files are in sickrage install location for items Cache Folder, failed.db,sickbeard.db, and cache.db
 cd
-tar xjf /home/xxxusernamexxx/install/SickRage/sickrage.tar.bz2
-cp -r /home/xxxusernamexxx/sickrage/* /opt/SickRage/
+tar xjf /opt/install/SickRage/sickrage.tar.bz2 -C /opt
+cp -rf /opt/sickrage/* /opt/SickRage
+rm -rf /opt/sickrage
 
 sudo chown -R sickrage:sickrage /opt/SickRage
 sudo chmod -R 0777 /opt/SickRage
 
 echo "Creating Startup Script"
-cp /home/xxxusernamexxx/install/SickRage/sickrage.service /etc/systemd/system/
+cp /opt/install/SickRage/sickrage.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/sickrage.service
 systemctl enable sickrage.service
 systemctl restart sickrage.service
