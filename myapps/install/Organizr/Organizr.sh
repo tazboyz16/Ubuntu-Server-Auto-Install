6be7862b4e9c1cb9315ea44e794b9202710c9d3e
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 02:31:08 -0400

    Update Organizr.sh

--- myapps/install/Organizr/Organizr.sh
+++ myapps/install/Organizr/Organizr.sh
@@ -2,14 +2,17 @@
 
 sudo adduser --disabled-password --system --home /opt/ProgramData/Organizer --gecos "Organizr Service" --group Organizr
 
+sudo bash /opt/install/Apache2/Apache2-install.sh
+
+sudo apt install php7.0 php7.0-* openssl libapache2-mod-php7.0 -y
+apt-get remove php7.0-snmp -yy
+
 cd /opt && git clone https://github.com/causefx/Organizr.git
 
 sudo chown -R Organizr:Organizr /opt/Organizr
 sudo chmod -R 0777 /opt/Organizr
 
+sudo cp /opt/install/Organizr/Organizr.conf /etc/apache2/sites-available/
 
-echo "Creating Startup Script" 
-cp /home/xxxusernamexxx/install/Organizr/Organizr.service /etc/systemd/system/
-chmod 644 /etc/systemd/system/Organizr.service
-systemctl enable Organizr.service
-systemctl restart Organizr.service
+sudo a2ensite Organizr.conf
+sudo service apache2 restart
