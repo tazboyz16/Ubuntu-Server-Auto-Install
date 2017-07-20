3c6ea7a280e5e9be678e5d5e2f17f2b3ee8382ae
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 12:00:53 -0400

    Update Muximuxinstall.sh

--- myapps/install/Muximux/Muximuxinstall.sh
+++ myapps/install/Muximux/Muximuxinstall.sh
@@ -1,7 +1,18 @@
-# /usr/bash
+#!/bin/bash
 
-sudo chmod 0777 -R /var/www
-git clone https://github.com/mescon/Muximux /var/www/html
+sudo adduser --disabled-password --system --home /opt/ProgramData/Muximux --gecos "Muximux Service" --group Muximux
 
-sudo chmod 0777 -R /var/www/html/Muximux
-sudo chown www-data:www-data -R /var/www/html/Muximux
+sudo bash /opt/install/Apache2/Apache2-install.sh
+
+sudo apt install php7.0 php7.0-* openssl libapache2-mod-php7.0 -y
+apt-get remove php7.0-snmp -yy
+
+cd /opt && git clone https://github.com/mescon/Muximux.git
+
+sudo chown -R Muximux:Muximux /opt/Muximux
+sudo chmod -R 0777 /opt/Muximux
+
+sudo cp /opt/install/Muximux/Muximux.conf /etc/apache2/sites-available/
+
+sudo a2ensite Muximux.conf
+sudo service apache2 restart
