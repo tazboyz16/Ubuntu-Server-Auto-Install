3bb19489c41aaab46a3309c8ca86ededec47c022
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 02:29:23 -0400

    Update plexinstall.sh

--- myapps/install/Plex/plexinstall.sh
+++ myapps/install/Plex/plexinstall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "<--- Installing Plex Media Server ->"
-bash /home/xxxusernamexxx/update/plexupdate.sh -p -a -d
+bash /opt/update/plexupdate.sh -p -a -d
 
-echo "Installing 3rd Party Addons"
-bash /home/xxxusernamexxx/install/PlexAddons/PlexAddons.sh
