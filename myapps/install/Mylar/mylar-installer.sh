27871cb6426f7f6bc08befaaa1318de62cbfb193
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 02:34:57 -0400

    Update mylar-installer.sh

--- myapps/install/Mylar/mylar-installer.sh
+++ myapps/install/Mylar/mylar-installer.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 echo '<--- Installing prerequisites for Mylar --->'
-sudo apt-get -yqq install git-core python python-cheetah python-pyasn1
+sudo apt install git-core python python-cheetah python-pyasn1 -y
 
 sudo adduser --disabled-password --system --home /opt/ProgramData/mylar --gecos "Mylar Service" --group mylar
 
 echo '<--- Downloading latest Mylar --->'
 cd /opt && sudo git clone https://github.com/evilhero/mylar.git
 
-#echo "<--- Restoring Mylar Settings --->"
-#cat /home/xxxusernamexxx/install/Mylar/Mylar.txt > /opt/mylar/config.ini
-
-echo "<-- enabling SSL --->"
-sed -i "s/https_cert = .*/https_cert = /etc/apache2/ssl/apache.crt/" /opt/mylar/config.ini
-sed -i "s/https_key = .*/https_key = /etc/apache2/ssl/apache.key/" /opt/mylar/config.ini
+echo "<--- Restoring Mylar Settings --->"
+cat /opt/install/Mylar/Mylar.txt > /opt/mylar/config.ini
 
 sudo chown -R mylar:mylar /opt/mylar
 sudo chmod -R 0777 /opt/mylar
 
 echo "Creating Startup Script"
-cp /home/xxxusernamexxx/install/Mylar/mylar.service /etc/systemd/system/
+cp /opt/install/Mylar/mylar.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/mylar.service
 systemctl enable mylar.service
 systemctl restart mylar.service
