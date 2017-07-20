95dc2d0161dd7d2a17cefdd3fc87691b93ab5e3b
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 02:22:11 -0400

    Update plexpy.sh

--- myapps/install/PlexAddons/plexpy.sh
+++ myapps/install/PlexAddons/plexpy.sh
@@ -2,17 +2,13 @@
 
 echo "Installing PlexPy"
 sudo adduser --disabled-password --system --no-create-home --gecos "PlexPy Service"  --group plexpy
-
 cd /opt && sudo git clone https://github.com/JonnyWong16/plexpy.git
-
 #echo "<--- Restoring PLexPy Settings --->"
-#cat /home/xxxusernamexxx/install/PlexAddons/Headphones.txt > /home/xxxusernamexxx/.headphones/config.ini
-
+#cat /opt/install/PlexAddons/Headphones.txt > /opt/.headphones/config.ini
 sudo chown -R plexpy:plexpy /opt/plexpy
 sudo chmod -R 0777 /opt/plexpy
-
 echo "Creating Startup Script for PlexPy"
-cp /home/xxxusernamexxx/install/PlexAddons/plexpy.service /etc/systemd/system/
+cp /opt/install/PlexAddons/plexpy.service /etc/systemd/system/
 chmod 0777 /etc/systemd/system/plexpy.service
 systemctl enable plexpy.service
 systemctl restart plexpy.service
