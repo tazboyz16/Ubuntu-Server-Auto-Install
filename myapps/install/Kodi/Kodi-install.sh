2db64ecf21631ab7e8d33733959db5de5640b270
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-20 21:35:05 -0400

    Update Kodi-install.sh

--- myapps/install/Kodi/Kodi-install.sh
+++ myapps/install/Kodi/Kodi-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #install is only for desktop environment 
+#https://forum.kodi.tv/showthread.php?tid=210762 - non desktop gui
 sudo add-apt-repository -y ppa:team-xbmc/ppa
 sudo apt-get update
 sudo apt-get install -yqq kodi
