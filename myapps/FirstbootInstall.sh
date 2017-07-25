1217c8d077377bed7c7a62aab5b3c90956e9b5fe
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-25 18:35:01 -0400

    Update FirstbootInstall.sh

--- myapps/FirstbootInstall.sh
+++ myapps/FirstbootInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 echo "<---Updaing System....Please Wait..--->"
 sudo apt-get update -qq
 sudo apt-get upgrade -yqq
