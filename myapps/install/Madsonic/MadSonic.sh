4c8a8a447fb13d5f08167135ee5cb298a60cc096
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-25 23:14:35 -0400

    Update MadSonic.sh

--- myapps/install/Madsonic/MadSonic.sh
+++ myapps/install/Madsonic/MadSonic.sh
@@ -2,5 +2,6 @@
 sudo apt-get install openjdk-8-jre
 
 #Latest Stable as of 5/29/2017
+#http://beta.madsonic.org/pages/download.jsp
 wget http://madsonic.org/download/6.2/20161208_madsonic-6.2.9040.deb
 sudo dpkg -i 20161208_madsonic-6.2.9040.deb
