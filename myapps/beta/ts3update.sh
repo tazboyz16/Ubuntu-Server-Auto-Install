bb3b63c2fc52b2b302664a5c1c2fed02ac6f71a2
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-26 20:58:11 -0400

    Update ts3update.sh

--- myapps/beta/ts3update.sh
+++ myapps/beta/ts3update.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+#Modes 
+# b=backup i=install ri=reinstall r=restore u=update
+mode=i
 server=/opt/ts3
-backup=$server-backup
+backupdir=/opt/backup/ts3
 dl=/tmp
+BK=$(cat $server/version)
 
+echo "Preforming System Check Up and Getting Latest Version Number from TeamSpeak"
 arch=`uname -m`
 if [ "$arch" == "x86_64" ]; then
     arch="amd64"
@@ -11,57 +16,80 @@ else
     arch="x86"
 fi
 
-echo "Getting version number from teamspeak.com"
+#making sure Backup Dir exist
+mkdir $backupdir
+
 wget -q https://www.teamspeak.com/downloads --output-document=$dl/Temp
 Version=$(grep -Pom 1 "server_linux_$arch-\K.*?(?=\.tar\.bz)" $dl/Temp)
 rm $dl/Temp
-
 if [ "$Version" == "" ]; then
     echo "Failed to get version!"
     exit
 fi
 
-if [ -a $server/version ]; then
-    BK=$(cat $server/version)
-else
-    echo "Local server version not found, referring to backups"
-    BK=$(ls $backup/ | cut -d':' -f1 | sort -n | tail -1)
-    BK="$BK(1)"
-    if [ "$BK" == "" ]; then
-        echo 'Backups not found, creating backup directory defaulting to "Backup 1"'
-        mkdir $backup
-        BK="Backup 1"
-    fi
-fi
-
-if [ "$1" == "check" ]; then
-        echo "Server is up to date. (Version: $BK)"
-        exit
-elif [ "$BK" == "$Version" ]; then
-    read -p "Server is up to date! ($Version) Update anyways? [y/n]" yn
-    case $yn in
-        [Yy]* );;
-        * ) echo "Exiting..." &&  exit ;;
-    esac
-fi
-
-echo "Updating from $BK to $Version..."
-
-while true; do
-    read -p "Do you wish to backup the existing server? [y/n]" yn
-    case $yn in
-        [Yy]* ) echo "Creating backup $BK" && cp -rf $server $backup/$BK && break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer [y]es or [n]o.";;
-    esac
+#Checking if there was any Arguments with starting the script
+while true;
+do
+case "$1" in
+    (-b) backup;;
+    (-i) install;;
+    (-r) restore;;
+    (-ri) reinstall;;
+    (-u) update;;
+    (-*) echo "Invalid Argument"; exit 0;;
+    (*) break;;
+esac
 done
 
-if [ "$1" == "backup" ]; then
-    exit
-fi
+backup() {
 
+}
+
+install() {
+sudo ln -s /opt/ts3/redist/libmariadb.so.2 /opt/ts3/libmariadb.so.2
+
+
+
+echo "Creating Startup Script"
+cp /opt/install/TeamSpeak3/ts3.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/ts3.service
+systemctl enable ts3.service
+systemctl restart ts3.service
+}
+
+restore() {
+
+}
+
+reinstall() {
+
+}
+
+update() {
+if [ "$BK" == "$Version" ]; then
+    echo= "Server is up to date! Latest Version is ($Version). If you want to still install over run Script with -ri"
+fi
+systemctl stop ts3
+
+echo "Running Backup of Settings and DB of TeamSpeak Server Before Update"
+cp $server/query_ip_blacklist.txt $backupdir
+cp $server/query_ip_whitelist.txt $backupdir
+cp $server/ts3server.ini $backupdir
+cp $server/ts3db_mariadb.ini $backupdir
+cp $server/ts3server.sqlitedb $backupdir
+
+
+
+echo $Version > $server/version
+echo "Starting Updated Server"
+systemctl start ts3
+}
+
+exit 0
+
+######################################
 #Store variable for latest backup available
-BK=$(ls $backup/ | cut -d':' -f1 | sort -n | tail -1)
+#BK=$(ls $backup/ | cut -d':' -f1 | sort -n | tail -1)
 
 $server/ts3server_startscript.sh stop
 rm -rf $server
