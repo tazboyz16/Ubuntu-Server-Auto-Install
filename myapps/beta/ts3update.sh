51abed1c9dc5a185770b638c612ae809ef510aed
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-26 21:48:36 -0400

    Update ts3update.sh

--- myapps/beta/ts3update.sh
+++ myapps/beta/ts3update.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
 #Modes 
-# b=backup i=install ri=reinstall r=restore u=update
+# b=backup i=install ri=reinstall r=restore u=update U=Force Update
 mode=i
 server=/opt/ts3
 backupdir=/opt/backup/ts3
 dl=/tmp
 BK=$(cat $server/version)
+force=0
 
 echo "Preforming System Check Up and Getting Latest Version Number from TeamSpeak"
+#System Specs
 arch=`uname -m`
 if [ "$arch" == "x86_64" ]; then
     arch="amd64"
@@ -19,6 +21,7 @@ fi
 #making sure Backup Dir exist
 mkdir $backupdir
 
+#Getting Current Version Info
 wget -q https://www.teamspeak.com/downloads --output-document=$dl/Temp
 Version=$(grep -Pom 1 "server_linux_$arch-\K.*?(?=\.tar\.bz)" $dl/Temp)
 rm $dl/Temp
@@ -36,6 +39,7 @@ case "$1" in
     (-r) restore;;
     (-ri) reinstall;;
     (-u) update;;
+    (-U) force=1; update;;
     (-*) echo "Invalid Argument"; exit 0;;
     (*) break;;
 esac
@@ -67,8 +71,18 @@ reinstall() {
 
 update() {
 if [ "$BK" == "$Version" ]; then
-    echo= "Server is up to date! Latest Version is ($Version). If you want to still install over run Script with -ri"
+    case $force in
+    0)
+    echo= "Server is up to date! Latest Version is ($Version). If you want to still install over Installation"
+    echo= "Run Script with '-U' to Force Update"
+    exit 0
+    ;;
+    *)
+    ;;
+    esac
 fi
+
+echo "Stopping TS3 Server"
 systemctl stop ts3
 
 echo "Running Backup of Settings and DB of TeamSpeak Server Before Update"
@@ -78,7 +92,20 @@ cp $server/ts3server.ini $backupdir
 cp $server/ts3db_mariadb.ini $backupdir
 cp $server/ts3server.sqlitedb $backupdir
 
+echo "Downloading Latest Version of TeamSpeak 3 Server"
+wget -nv http://teamspeak.gameserver.gamed.de/ts3/releases/$Version/teamspeak3-server_linux_$arch-$Version.tar.bz2 --output-document=$dl/package.tar.bz2
 
+echo "Installing TS3 Server Version $Version"
+tar -xjf $dl/package.tar.bz2 -C $dl/
+mv $dl/teamspeak3-server_linux_$arch $server
+rm $dl/package.tar.bz2
+
+echo "Moving back Settings and DB back to TS3 Folder"
+mv $backupdir/query_ip_blacklist.txt $server
+mv $backupdir/query_ip_whitelist.txt $server
+mv $backupdir/ts3server.ini $server
+mv $backupdir/ts3db_mariadb.ini $server
+mv $backupdir/ts3server.sqlitedb $server
 
 echo $Version > $server/version
 echo "Starting Updated Server"
