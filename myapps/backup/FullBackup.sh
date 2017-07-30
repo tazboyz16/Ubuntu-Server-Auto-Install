5e7ca9859a7a2f48fe90144cb278769c36a99ec0
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-29 23:38:30 -0400

    Update and rename a to FullBackup.sh

---
+++ myapps/backup/FullBackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#/opt/Backup  location
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
+#SickRage
+cd /opt/SickRage
+sudo tar -cvjSf SickRage.tar.bz2 *
+
