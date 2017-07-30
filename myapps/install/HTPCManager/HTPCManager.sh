49ffde5f951580360aec69a80f3c79b838b4cbe6
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-30 14:23:00 -0400

    Update HTPCManager.sh

--- myapps/install/HTPCManager/HTPCManager.sh
+++ myapps/install/HTPCManager/HTPCManager.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install libjpeg-dev libpng12-dev libfreetype6-dev zlib1g-dev
-#sudo apt-get install python-pip
-#sudo pip install PIL
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
+sudo apt install libjpeg-dev libpng12-dev libfreetype6-dev zlib1g-dev libc6-dev libc-dev libjpeg8-dev python -y
 
 sudo adduser --disabled-password --system --home /opt/ProgramData/HTPC-Manager --gecos "HTPC-Manager Service" --group HTPCManager
 
@@ -12,7 +15,7 @@ sudo chown -R HTPCManager:HTPCManager /opt/HTPC-Manager
 sudo chmod -R 0777 /opt/HTPC-Manager
 
 echo "Creating Startup Script"
-cp /home/xxxusernamexxx/install/HTPCManager/HTPCManager.service /etc/systemd/system/
+cp /opt/install/HTPCManager/HTPCManager.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/HTPCManager.service
 systemctl enable HTPCManager.service
 systemctl restart HTPCManager.service
