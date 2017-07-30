e62b4df43bb0e6f47f6097b9583359bf9957349e
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-30 14:25:00 -0400

    Update headphones-installer.sh

--- myapps/install/Headphones/headphones-installer.sh
+++ myapps/install/Headphones/headphones-installer.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
 sudo adduser --disabled-password --system --home /opt/ProgramData/headphones --gecos "Headphones Service" --group headphones
 
 echo '<--- Downloading latest Headphones --->'
 cd /opt &&  git clone https://github.com/rembo10/headphones.git
 
-#echo "<--- Restoring Headphones Settings --->"
-#cat /home/xxxusernamexxx/install/Headphones/Headphones.txt > /opt/headphones/config.ini
+apt install python git-core -y
+
+echo "<--- Restoring Headphones Settings --->"
+#cat /opt/install/Headphones/Headphones.txt > /opt/headphones/config.ini
 
 sudo chown -R headphones:headphones /opt/headphones
 sudo chmod -R 0777 /opt/headphones
 
 echo "Creating Startup Script"
-cp /home/xxxusernamexxx/install/Headphones/headphones.service /etc/systemd/system/
+cp /opt/install/Headphones/headphones.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/headphones.service
 systemctl enable headphones.service
 systemctl restart headphones.service
