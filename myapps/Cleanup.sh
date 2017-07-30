f7721447a89537e6a1376431d6c5a94c47cea927
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-29 23:36:46 -0400

    Update Cleanup.sh

--- myapps/Cleanup.sh
+++ myapps/Cleanup.sh
@@ -7,11 +7,6 @@ fi
 
 echo "<---- Cleaning Up Install files --->"
 rm -r /opt/WebTools.bundle/
-rm -r /opt/install/
-rm -r /opt/ts3/
-rm -r /opt/iRedMail-*
-rm -r /opt/sinusbot/
-rm -r /opt/Jackett
 
 sudo chmod 0777 -R /opt/
 sudo apt-get update -qq
