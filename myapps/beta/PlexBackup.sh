9a81e5e5dab28a7974b56e469bf7449a9ff6d784
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-29 23:42:40 -0400

    Update PlexBackup.sh

--- myapps/beta/PlexBackup.sh
+++ myapps/beta/PlexBackup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
 sudo systemctl stop plexmediaserver
 cd /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/
 
-sudo tar -cvpzf /media/SystemBackup/PlexBackup.tgz.bz2 *  
+sudo tar -cvpzf /opt/backup/PlexBackup.tgz.bz2 *  
 
 sudo systemctl start plexmediaserver
