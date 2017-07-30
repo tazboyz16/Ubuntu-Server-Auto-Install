23e06afa61d3bad970825e6d78b4c7c1526f4d41
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-30 01:56:15 -0400

    Update Apache2-install.sh

--- myapps/install/Apache2/Apache2-install.sh
+++ myapps/install/Apache2/Apache2-install.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-apt-get install -yqq apache2
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
+apt install apache2 -y
 sleep 15
+
 echo "<--- Restoring Apache2 Settings --->"
-cat /home/xxxusernamexxx/install/Apache2/apache2.conf > /etc/apache2/apache2.conf
+cat /opt/install/Apache2/apache2.conf > /etc/apache2/apache2.conf
 sleep 5
 echo "<- Restoring Apache2 Error Pages ->"
-cat /home/xxxusernamexxx/install/Apache2/localized-error-pages.conf > /etc/apache2/conf-available/localized-error-pages.conf
+cat /opt/install/Apache2/localized-error-pages.conf > /etc/apache2/conf-available/localized-error-pages.conf
 systemctl restart apache2
 
-#echo "<--- Restoring WWW files for Website --->"
-#rm -rf /var/www
-#cd
-#tar xjf /home/taz/install/Apache2/www.tar.bz2
-#mv /home/xxxusernamexxx/www/ /var
+echo "<--- Restoring WWW files for Website --->"
+rm -rf /var/www
+cd /opt/backup
+tar xjf /opt/install/Apache2/www.tar.bz2
+mv /opt/backup/www/ /var
+
 echo
 echo "<--- Changing Rights for dir WWW --->"
 chmod 0777 -R /var/www
