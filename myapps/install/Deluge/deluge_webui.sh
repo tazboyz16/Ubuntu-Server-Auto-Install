2ecc2dbb4742f2c5d16281cb1df16bfca7c20322
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-30 14:06:06 -0400

    Update deluge_webui.sh

--- myapps/install/Deluge/deluge_webui.sh
+++ myapps/install/Deluge/deluge_webui.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
 ## get packages required to build deluge ##
 echo "<--- Adding Deluge Team Dep Packages--->"
 sudo add-apt-repository -y ppa:deluge-team/ppa
@@ -13,11 +18,11 @@ sudo touch /var/log/deluged.log
 sudo touch /var/log/deluge-web.log
 sudo chown deluge:deluge /var/log/deluge*
 
-sudo apt-get update -qq && sudo apt-get install -yqq deluged deluge-webui
+sudo apt-get update -qq && sudo apt install deluged deluge-webui -y
 
 echo "Creating New Startup Script"
-cp /home/xxxusernamexxx/install/Deluge/deluged.service /etc/systemd/system/
-cp /home/xxxusernamexxx/install/Deluge/deluge-web.service /etc/systemd/system/
+cp /opt/install/Deluge/deluged.service /etc/systemd/system/
+cp /opt/install/Deluge/deluge-web.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/deluged.service
 chmod 644 /etc/systemd/system/deluge-web.service
 systemctl enable deluged.service
@@ -32,17 +37,8 @@ echo "<--Restoring Deluge Settings and Switch to Systemctl startup scripts-->"
 systemctl stop deluged
 systemctl stop deluge-web
 sleep 15
-
-#SSL Cert
-# "pkey": "/etc/apache2/ssl/apache.key", 
-# "cert": "/etc/apache2/ssl/apache.crt",
-#sed -i "s/ssl_key = .*/ssl_key = /etc/apache2/ssl/apache.key" /var/lib/deluge/.config/deluge/web.conf
-#sed -i "s/ssl_cert = .*/ssl_cert = /etc/apache2/ssl/apache.crt/" /var/lib/deluge/.config/deluge/web.conf
-
-#uncomment below lines for backup settings to be restored
-#sudo chmod 0777 -R /var/lib/deluge/
-#cp /home/xxxusernamexxx/install/Deluge/core.conf /var/lib/deluge/.config/deluge
-#cp /home/xxxusernamexxx/install/Deluge/web.conf /var/lib/deluge/.config/deluge
-
+sudo chmod 0777 -R /var/lib/deluge/
+cp /opt/install/Deluge/core.conf /var/lib/deluge/.config/deluge
+cp /opt/install/Deluge/web.conf /var/lib/deluge/.config/deluge
 systemctl start deluged
 systemctl start deluge-web
