8cb40b4a9fe0bd434c63f5c0623c62b96a71a5dc
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-30 20:21:44 -0400

    Update Mopidy.sh

--- myapps/beta/Mopidy.sh
+++ myapps/beta/Mopidy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
 sudo apt-get update
