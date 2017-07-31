14d93f40f38ff6128f7fc5c6516738b09cc6b009
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-31 10:24:20 -0400

    Update ts3update-beta.sh

--- myapps/update/ts3update-beta.sh
+++ myapps/update/ts3update-beta.sh
@@ -13,7 +13,6 @@ server=/opt/ts3
 backupdir=/opt/backup/ts3
 dl=/tmp
 BK=$(cat $server/version)
-force=0
 time=$(date +"%m_%d_%y-%H_%M")
 
 echo "Preforming System Check Up and Getting Latest Version Number from TeamSpeak"
@@ -56,7 +55,7 @@ case $mode in
 			wget -nv http://teamspeak.gameserver.gamed.de/ts3/releases/$Version/teamspeak3-server_linux_$arch-$Version.tar.bz2 --output-document=$dl/package.tar.bz2
 			echo "Installing TS3 Server Version $Version"
 			tar -xjf $dl/package.tar.bz2 -C $dl/
-			mv $dl/teamspeak3-server_linux_$arch $server
+			mv $dl/teamspeak3-server_linux_$arch/* $server
 			rm $dl/package.tar.bz2
 			sudo ln -s /opt/ts3/redist/libmariadb.so.2 /opt/ts3/libmariadb.so.2
 			sudo touch /opt/ts3/query_ip_blacklist.txt
@@ -75,7 +74,7 @@ case $mode in
     echo "Stopping TS3 Server"
 		systemctl stop ts3
 		echo "Location with the file name and type of the backup to be restored?"
-		echo "Ex. /opt/backup/ts3-xxx.tar.bz2"
+		echo "Ex. /opt/backup/ts3_FullBackup-xxx.tar.gz"
 		read restorebackup
 		echo "Untar Zip file to /opt/ts3"
 		tar -xjf $restorebackup /opt/ts3
@@ -90,12 +89,53 @@ case $mode in
 		read rianswer
     	case $rianswer in
         Clean)
-        rm -rf $server
-        install;
-        exit 1;;
+        	rm -rf $server
+          echo "Creating Teamspeak User account"
+					sudo adduser --no-create-home --disabled-password --gecos "TeamSpeak Server" teamspeak
+					echo "Downloading Latest Version of TeamSpeak 3 Server"
+					wget -nv http://teamspeak.gameserver.gamed.de/ts3/releases/$Version/teamspeak3-server_linux_$arch-$Version.tar.bz2 --output-document=$dl/package.tar.bz2
+					echo "Installing TS3 Server Version $Version"
+					tar -xjf $dl/package.tar.bz2 -C $dl/
+					mv $dl/teamspeak3-server_linux_$arch/* $server
+					rm $dl/package.tar.bz2
+					sudo ln -s /opt/ts3/redist/libmariadb.so.2 /opt/ts3/libmariadb.so.2
+					sudo touch /opt/ts3/query_ip_blacklist.txt
+					echo "127.0.0.1" > /opt/ts3/query_ip_whitelist.txt
+					cat /opt/install/TeamSpeak3/ts3server.txt > /opt/ts3/ts3server.ini
+					cat /opt/install/TeamSpeak3/ts3db_mariadb.txt > /opt/ts3/ts3db_mariadb.ini
+					chmod 0777 /opt/ts3 -R
+					chown teamspeak:teamspeak /opt/ts3 -R
+					echo "Creating Startup Script"
+					cp /opt/install/TeamSpeak3/ts3.service /etc/systemd/system/
+					chmod 644 /etc/systemd/system/ts3.service
+					systemctl enable ts3.service
+					systemctl restart ts3.service
+        	exit 0;;
         Reinstall)
-        update;
-        exit 1;;
+        	echo "Stopping TS3 Server"
+					systemctl stop ts3
+					echo "Running Backup of Settings and DB of TeamSpeak Server Before Update"
+					cp $server/query_ip_blacklist.txt $backupdir
+					cp $server/query_ip_whitelist.txt $backupdir
+					cp $server/ts3server.ini $backupdir
+					cp $server/ts3db_mariadb.ini $backupdir
+					cp $server/ts3server.sqlitedb $backupdir
+					echo "Downloading Latest Version of TeamSpeak 3 Server"
+					wget -nv http://teamspeak.gameserver.gamed.de/ts3/releases/$Version/teamspeak3-server_linux_$arch-$Version.tar.bz2 --output-document=$dl/package.tar.bz2
+					echo "Installing TS3 Server Version $Version"
+					tar -xjf $dl/package.tar.bz2 -C $dl/
+					cp -rf $dl/teamspeak3-server_linux_$arch/* $server
+					rm $dl/package.tar.bz2
+					echo "Moving back Settings and DB back to TS3 Folder"
+					mv $backupdir/query_ip_blacklist.txt $server
+					mv $backupdir/query_ip_whitelist.txt $server
+					mv $backupdir/ts3server.ini $server
+					mv $backupdir/ts3db_mariadb.ini $server
+					mv $backupdir/ts3server.sqlitedb $server
+					echo $Version > $server/version
+					echo "Starting Updated Server"
+					systemctl start ts3
+  	      exit 0;;
     	esac
 		echo "Starting TS3 Server"
 		systemctl start ts3 
@@ -129,7 +169,6 @@ case $mode in
 		echo $Version > $server/version
 		echo "Starting Updated Server"
 		systemctl start ts3
-		exit 0
 		;;
     (-U) 
     echo "Stopping TS3 Server"
@@ -155,9 +194,7 @@ case $mode in
 		echo $Version > $server/version
 		echo "Starting Updated Server"
 		systemctl start ts3
-		exit 0
     ;;
     (-*) echo "Invalid Argument"; exit 0;;
 esac
-install;
 exit 0
