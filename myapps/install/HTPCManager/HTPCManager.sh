8f9eb43d8351b2e7833f661712cc64001c84079e
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 03:20:39 -0400

    Update HTPCManager.sh

--- myapps/install/HTPCManager/HTPCManager.sh
+++ myapps/install/HTPCManager/HTPCManager.sh
@@ -5,14 +5,13 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-sudo apt install libjpeg-dev libpng12-dev libfreetype6-dev zlib1g-dev libc6-dev libc-dev libjpeg8-dev python -y
+apt install libjpeg-dev libpng12-dev libfreetype6-dev zlib1g-dev libc6-dev libc-dev libjpeg8-dev python -y
 
-sudo adduser --disabled-password --system --home /opt/ProgramData/HTPC-Manager --gecos "HTPC-Manager Service" --group HTPCManager
+adduser --disabled-password --system --home /opt/ProgramData/HTPC-Manager --gecos "HTPC-Manager Service" --group HTPCManager
 
 cd /opt &&  git clone https://github.com/styxit/HTPC-Manager.git
-
-sudo chown -R HTPCManager:HTPCManager /opt/HTPC-Manager
-sudo chmod -R 0777 /opt/HTPC-Manager
+chown -R HTPCManager:HTPCManager /opt/HTPC-Manager
+chmod -R 0777 /opt/HTPC-Manager
 
 echo "Creating Startup Script"
 cp /opt/install/HTPCManager/HTPCManager.service /etc/systemd/system/
