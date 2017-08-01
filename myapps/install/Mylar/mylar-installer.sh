df9ce6bfcedec95b37e2b3375f5117a0304b1b5c
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 03:31:30 -0400

    Update mylar-installer.sh

--- myapps/install/Mylar/mylar-installer.sh
+++ myapps/install/Mylar/mylar-installer.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 echo '<--- Installing prerequisites for Mylar --->'
-sudo apt install git-core python python-cheetah python-pyasn1 -y
+apt install git-core python python-cheetah python-pyasn1 -y
 
-sudo adduser --disabled-password --system --home /opt/ProgramData/mylar --gecos "Mylar Service" --group mylar
+adduser --disabled-password --system --home /opt/ProgramData/mylar --gecos "Mylar Service" --group mylar
 
 echo '<--- Downloading latest Mylar --->'
 cd /opt && sudo git clone https://github.com/evilhero/mylar.git
 
 echo "<--- Restoring Mylar Settings --->"
 cat /opt/install/Mylar/Mylar.txt > /opt/mylar/config.ini
-
-sudo chown -R mylar:mylar /opt/mylar
-sudo chmod -R 0777 /opt/mylar
+chown -R mylar:mylar /opt/mylar
+chmod -R 0777 /opt/mylar
 
 echo "Creating Startup Script"
 cp /opt/install/Mylar/mylar.service /etc/systemd/system/
