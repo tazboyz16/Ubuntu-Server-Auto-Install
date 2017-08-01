f1d38790a86ef806ab04f61dcc1e4f7b76e35948
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 02:36:50 -0400

    Update Mopidy.sh

--- myapps/beta/Mopidy.sh
+++ myapps/beta/Mopidy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
-sudo apt-get update
-sudo apt-get install mopidy
+wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
+apt get update; apt get install mopidy
+
 #~/.config/mopidy/mopidy.conf
-
-
 #mopidy config to effective configuration
 #https://docs.mopidy.com/en/latest/service/#service
 
-
-sudo pip install Mopidy-Moped
+pip install Mopidy-Moped
 # http://localhost:6680/moped
