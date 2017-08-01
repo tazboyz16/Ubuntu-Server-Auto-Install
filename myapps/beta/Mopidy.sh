f08a00736f5fd4e727896a0a1c5524d7997b354b
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 02:39:49 -0400

    Update Mopidy.sh

--- myapps/beta/Mopidy.sh
+++ myapps/beta/Mopidy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
 apt get update; apt get install mopidy
