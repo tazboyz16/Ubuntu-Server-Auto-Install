65ae8c3da2c2748f29335d5f90a0289301d6a305
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 03:27:48 -0400

    Update MadSonic.sh

--- myapps/install/Madsonic/MadSonic.sh
+++ myapps/install/Madsonic/MadSonic.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt-get install openjdk-8-jre
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+apt get install openjdk-8-jre -y
 
 #Latest Stable as of 5/29/2017
 #http://beta.madsonic.org/pages/download.jsp
 wget http://madsonic.org/download/6.2/20161208_madsonic-6.2.9040.deb
-sudo dpkg -i 20161208_madsonic-6.2.9040.deb
+dpkg -i 20161208_madsonic-6.2.9040.deb
