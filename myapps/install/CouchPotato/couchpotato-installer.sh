0f5c9e022542ba254886239824e54afdbcd298f9
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-01 23:42:00 -0400

    Update couchpotato-installer.sh

--- myapps/install/CouchPotato/couchpotato-installer.sh
+++ myapps/install/CouchPotato/couchpotato-installer.sh
@@ -5,21 +5,38 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+#Modes (Variables)
+# b=backup i=install r=restore u=update U=Force Update 
+mode="$1"
+
+Programloc=/opt/CouchPotatoServer/
+backupdir=/opt/backup/CouchPotatoServer
+dl=/tmp
+time=$(date +"%m_%d_%y-%H_%M")
 apt install git-core python python-cheetah python-pyasn1 python3-lxml -y
 
-adduser --disabled-password --system --home /opt/ProgramData/couchpotato --gecos "CouchPotato Service" --group couchpotato
+case $mode in
+	(-i)
+	apt install git-core python python-cheetah python-pyasn1 python3-lxml -y
+	adduser --disabled-password --system --home /opt/ProgramData/couchpotato --gecos "CouchPotato Service" --group couchpotato
+	echo "<--- Downloading latest CouchPotato --->"
+	cd /opt && sudo git clone https://github.com/CouchPotato/CouchPotatoServer.git
+	chown -R couchpotato:couchpotato /opt/CouchPotatoServer/
+	chmod -R 0777 /opt/CouchPotatoServer
+	echo "Creating Startup Script"
+	cp /opt/install/CouchPotato/couchpotato.service /etc/systemd/system/
+	chmod 644 /etc/systemd/system/couchpotato.service
+	systemctl enable couchpotato.service
+	systemctl restart couchpotato.service 
+	;;
+	(-r)
+	echo "<--- Restoring CouchPotato Settings --->"
+	chmod -R 0777 /opt/ProgramData/couchpotato
+	cp /opt/install/CouchPotato/CouchPotato.txt /opt/ProgramData/couchpotato/.couchpotato/settings.conf
+	;;
+	
+	
 
-echo "<--- Downloading latest CouchPotato --->"
-cd /opt && sudo git clone https://github.com/CouchPotato/CouchPotatoServer.git
-chown -R couchpotato:couchpotato /opt/CouchPotatoServer/
-chmod -R 0777 /opt/CouchPotatoServer
-
-echo "<--- Restoring CouchPotato Settings --->"
-chmod -R 0777 /opt/ProgramData/couchpotato
-#cp /opt/install/CouchPotato/CouchPotato.txt /opt/ProgramData/couchpotato/.couchpotato/settings.conf
-
-echo "Creating Startup Script"
-cp /opt/install/CouchPotato/couchpotato.service /etc/systemd/system/
-chmod 644 /etc/systemd/system/couchpotato.service
-systemctl enable couchpotato.service
-systemctl restart couchpotato.service 
+    	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0
