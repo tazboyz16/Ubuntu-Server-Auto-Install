a3476518d0b8f185581af83a0734edb0ed30ca81
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-03 02:26:13 -0400

    Update EmbyServerInstall.sh

--- myapps/install/EmbyServer/EmbyServerInstall.sh
+++ myapps/install/EmbyServer/EmbyServerInstall.sh
@@ -3,9 +3,11 @@
 version=$(lsb_release -rs)
 echo "<--- Adding Emby to Repository --->"
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/emby/xUbuntu_$version/ /' > /etc/apt/sources.list.d/emby-server.list"
+echo "Grabbing Repository Key"
+wget -nv http://download.opensuse.org/repositories/home:emby/xUbuntu_$version/Release.key -O Release.key
+sudo apt-key add - < Release.key
 echo "<--- Installing Emby Server --->"
 apt update; apt install emby-server -y
 
 systemctl enable emby-server
-
 echo "<--- Finished Installing Emby Server --->"
