d5d2f1f2619daf834e02cc4335206f524230066c
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-03 00:55:08 -0400

    Update EmbyServerInstall.sh

--- myapps/install/EmbyServer/EmbyServerInstall.sh
+++ myapps/install/EmbyServer/EmbyServerInstall.sh
@@ -3,7 +3,7 @@
 echo "<--- Adding Emby to Repository --->"
 add-apt-repository "deb http://download.opensuse.org/repositories/home:emby/xUbuntu_16.04/ /"
 echo "<--- Installing Emby Server --->"
-apt get update; apt get install emby-server -y
+apt update; apt install emby-server -y
 
 systemctl enable emby-server
 
