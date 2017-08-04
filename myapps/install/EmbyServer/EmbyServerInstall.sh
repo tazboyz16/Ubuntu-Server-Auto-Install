803ca070d3ee1a4a19904c1ba54496c21300e7ec
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-03 23:13:21 -0400

    Update EmbyServerInstall.sh

--- myapps/install/EmbyServer/EmbyServerInstall.sh
+++ myapps/install/EmbyServer/EmbyServerInstall.sh
@@ -7,7 +7,8 @@ echo "Grabbing Repository Key"
 wget -nv http://download.opensuse.org/repositories/home:emby/xUbuntu_$version/Release.key -O Release.key
 sudo apt-key add - < Release.key
 echo "<--- Installing Emby Server --->"
-apt update; apt install emby-server -y
+apt update; apt install libmono-Corlib4.5-cli libmono-posix4.0-cli libmono-system-configuration4.0-cli libmono-system-core4.0-cli libmono-system-data4.0-cli libmono-system-runtime-serialization4.0-cli libmono-system-xml4.0-cli libmono-system4.0-cli mono-devel -y
+apt install emby-server -y
 
 systemctl enable emby-server
 echo "<--- Finished Installing Emby Server --->"
