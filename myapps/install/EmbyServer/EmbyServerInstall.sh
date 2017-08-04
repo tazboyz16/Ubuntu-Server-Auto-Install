d6d44958ca84e448a135adc804d8f627ff509fe5
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-03 23:23:32 -0400

    Update EmbyServerInstall.sh

--- myapps/install/EmbyServer/EmbyServerInstall.sh
+++ myapps/install/EmbyServer/EmbyServerInstall.sh
@@ -9,7 +9,7 @@ sudo apt-key add - < Release.key
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 echo "<--- Installing Emby Server --->"
-apt update; apt install libmono-Corlib4.5-cli libmono-posix4.0-cli libmono-system-configuration4.0-cli libmono-system-core4.0-cli libmono-system-data4.0-cli libmono-system-runtime-serialization4.0-cli libmono-system-xml4.0-cli libmono-system4.0-cli mono-devel -y
+apt update; apt install mono-complete -y
 apt install emby-server -y
 
 systemctl enable emby-server
