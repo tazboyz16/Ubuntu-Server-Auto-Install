9e46985dbbd21ea30e904e5cf8a280920a2f8ebd
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-04 00:36:11 -0400

    Remove due to Requires a Desktop GUI at all times

--- myapps/install/Kodi/Kodi-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Kodi Requires Desktop Gui to function
-#this bash script will install Kodi with its own Desktop gui on start up 
-
-cd /opt && git clone https://github.com/abacao/Kodi17-UbuntuServer.git
-cd Kodi17-UbuntuServer
-sudo ./install.sh
