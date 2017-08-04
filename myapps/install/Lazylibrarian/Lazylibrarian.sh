146b04038a21157ed23ed6a63d1c13a53d89d2b6
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-04 03:18:03 -0400

    Update Lazylibrarian.sh

--- myapps/install/Lazylibrarian/Lazylibrarian.sh
+++ myapps/install/Lazylibrarian/Lazylibrarian.sh
@@ -5,14 +5,66 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-adduser --disabled-password --system --home /opt/ProgramData/LazyLibrarian --gecos "LazyLibrarian Service" --group LazyLibrarian
+#Modes (Variables)
+# b=backup i=install r=restore 
+mode="$1"
 
-cd /opt &&  git clone https://github.com/DobyTang/LazyLibrarian.git
-chown -R LazyLibrarian:LazyLibrarian /opt/LazyLibrarian
-chmod -R 0777 /opt/LazyLibrarian
+Programloc=/opt/LazyLibrarian
+backupdir=/opt/backup/LazyLibrarian
+time=$(date +"%m_%d_%y-%H_%M")
 
-echo "Creating Startup Script" 
-cp /opt/install/Lazylibrarian/LazyLibrarian.service /etc/systemd/system/
-chmod 644 /etc/systemd/system/LazyLibrarian.service
-systemctl enable LazyLibrarian.service
-systemctl restart LazyLibrarian.service
+case $mode in
+	(-i|"")
+   adduser --disabled-password --system --home /opt/ProgramData/LazyLibrarian --gecos "LazyLibrarian Service" --group LazyLibrarian
+   cd /opt &&  git clone https://github.com/DobyTang/LazyLibrarian.git
+   chown -R LazyLibrarian:LazyLibrarian /opt/LazyLibrarian
+   chmod -R 0777 /opt/LazyLibrarian
+   echo "Creating Startup Script" 
+   cp /opt/install/Lazylibrarian/LazyLibrarian.service /etc/systemd/system/
+   chmod 644 /etc/systemd/system/LazyLibrarian.service
+   systemctl enable LazyLibrarian.service
+   systemctl restart LazyLibrarian.service
+   ;;
+   (-r)
+	echo "<--Restoring LazyLibrarian Settings -->"
+	echo "Stopping LazyLibrarian"
+	systemctl stop LazyLibrarian
+	sudo chmod 0777 -R $Programloc
+	cp /opt/install/Jackett/ServerConfig.json ~/.config/Jackett/
+	echo "Restarting up LazyLibrarian"
+	systemctl start LazyLibrarian
+	;;
+	(-b)
+	echo "Stopping LazyLibrarian"
+    	systemctl stop LazyLibrarian
+    	echo "Making sure Backup Dir exists"
+    	mkdir -p $backupdir
+    	echo "Backing up LazyLibrarian to /opt/backup"
+	cp ~/.config/Jackett/ServerConfig.json $backupdir
+	tar -zcvf /opt/backup/LazyLibrarian_FullBackup-$time.tar.gz $backupdir
+    	echo "Restarting up LazyLibrarian"
+	systemctl start LazyLibrarian
+	;;
+	(-u)
+	echo "Stopping LazyLibrarian to Update"
+	sudo systemctl stop LazyLibrarian
+	sleep 5
+	cd $Programloc
+	git pull
+	echo "Starting LazyLibrarian"
+	sudo systemctl start LazyLibrarian
+	;;
+	(-U)
+	echo "Stopping LazyLibrarian to Force Update"
+	sudo systemctl stop LazyLibrarian
+	sleep 5
+	cd $Programloc
+	git fetch --all
+	git reset --hard origin/master
+	git pull
+	echo "Starting LazyLibrarian"
+	sudo systemctl start LazyLibrarian
+	;;
+    	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0
