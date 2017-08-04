46d7a22256b9c10a7f0c44c14c477b61641b02ff
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-03 23:54:44 -0400

    Update EmbyServerInstall.sh

--- myapps/install/EmbyServer/EmbyServerInstall.sh
+++ myapps/install/EmbyServer/EmbyServerInstall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 version=$(lsb_release -rs)
+versionm=$(lsb_realease -cs)
 echo "<--- Adding Emby to Repository --->"
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/emby/xUbuntu_$version/ /' > /etc/apt/sources.list.d/emby-server.list"
 echo "Grabbing Repository Key"
 wget -nv http://download.opensuse.org/repositories/home:emby/xUbuntu_$version/Release.key -O Release.key
 sudo apt-key add - < Release.key
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 echo "<--- Installing Emby Server --->"
 apt update
 apt install mono-complete -y
