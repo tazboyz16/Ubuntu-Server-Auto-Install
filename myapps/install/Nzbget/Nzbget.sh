1a676156f08281757cfb749f44a7777132f72698
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-05 15:36:03 -0400

    Update Nzbget.sh

--- myapps/install/Nzbget/Nzbget.sh
+++ myapps/install/Nzbget/Nzbget.sh
@@ -11,3 +11,12 @@ sh nzbget-latest-bin-linux.run
 
 #Config file nzbget.conf
 
+Quick help (from nzbget-directory):
+   ./nzbget -s        - start nzbget in console mode
+   ./nzbget -D        - start nzbget in daemon mode (in background)
+   ./nzbget -C        - connect to background process
+   ./nzbget -Q        - stop background process
+   ./nzbget -h        - help screen with all commands
+
+Successfully installed into /home/cabox/workspace/nzbget
+Web-interface is on http://127.0.0.2:6789 (login:nzbget, password:tegbzn6789)
