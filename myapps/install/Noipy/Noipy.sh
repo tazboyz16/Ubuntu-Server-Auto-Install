a51bc99e0eecfb41f69f7eb29d34b89ca623fd88
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-05 09:48:24 -0400

    Update Noipy.sh

--- myapps/install/Noipy/Noipy.sh
+++ myapps/install/Noipy/Noipy.sh
@@ -5,7 +5,7 @@ apt install python python-pip
 pip install noipy
 
 #to create auth config file to service
-noipy --store -u tazboyz16 -p rg325odn -n tazserver.ga --provider generic --url https://api.dynu.com IP_ADDRESS
+noipy --store -u xxxusernamxxx -p xxxpasswordxxx -n xxxdomainxxx --provider generic --url https://api.dynu.com IP_ADDRESS
 
 #to run update
-noipy -n tazserver.ga --url https://api.dynu.com
+noipy -n xxxdomainxxx --url https://api.dynu.com
