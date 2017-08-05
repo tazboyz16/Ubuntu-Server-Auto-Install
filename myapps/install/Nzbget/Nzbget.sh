86acc5bd37d8ee6177c8c4268d707df01f000d87
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-05 15:04:07 -0400

    Update Nzbget.sh

--- myapps/install/Nzbget/Nzbget.sh
+++ myapps/install/Nzbget/Nzbget.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This Script must be run as root"
+	exit 1
+fi
+
 cd /opt & wget https://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run
 
