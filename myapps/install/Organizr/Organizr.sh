e578a3aa40d724860f8081d1ee9db39f3e8fe90d
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-06 09:58:24 -0400

    Update Organizr.sh

--- myapps/install/Organizr/Organizr.sh
+++ myapps/install/Organizr/Organizr.sh
@@ -5,15 +5,56 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-adduser --disabled-password --system --home /opt/ProgramData/Organizer --gecos "Organizr Service" --group Organizr
-bash /opt/install/Apache2/Apache2-install.sh
-apt install php7.0 php7.0-* openssl libapache2-mod-php7.0 -y
-apt get remove php7.0-snmp -y
+#Modes (Variables)
+# b=backup i=install r=restore u=update(coming soon)
+mode="$1"
+Programloc=/opt/Muximux
+backupdir=/opt/backup/Muximux
+time=$(date +"%m_%d_%y-%H_%M")
 
-cd /opt && git clone https://github.com/causefx/Organizr.git
-chown -R Organizr:Organizr /opt/Organizr
-chmod -R 0777 /opt/Organizr
-
-cp /opt/install/Organizr/Organizr.conf /etc/apache2/sites-available/
-a2ensite Organizr.conf
-service apache2 restart
+case $mode in
+	(-i|"")
+	echo "Setting up Organizr User"
+	adduser --disabled-password --system --home /opt/ProgramData/Organizer --gecos "Organizr Service" --group Organizr
+	bash /opt/install/Apache2/Apache2-install.sh
+	apt update
+	apt install php7.0 php7.0-* openssl libapache2-mod-php7.0 -y
+	apt remove php7.0-snmp -y
+	echo "Installing Organizr"
+	cd /opt && git clone https://github.com/causefx/Organizr.git
+	chown -R Organizr:Organizr /opt/Organizr
+	chmod -R 0777 /opt/Organizr
+	cp /opt/install/Organizr/Organizr.conf /etc/apache2/sites-available/
+	a2ensite Organizr.conf
+	service apache2 restart
+	;;
+	(-r)
+	echo "<--- Restoring Organizr Settings --->"
+	cat /opt/install/Organizr/Mylar.txt > /opt/Organizr/
+	chown -R Organizr:Organizr /opt/Organizr
+	chmod -R 0777 /opt/Organizr
+	;;
+	(-b)
+    	echo "Making sure Backup Dir exists"
+    	mkdir -p $backupdir
+    	echo "Backing up Organizr to /opt/backup"
+	cp -rf /opt/Organizr/ $backupdir
+    	tar -zcvf /opt/backup/Organizr_FullBackup-$time.tar.gz $backupdir
+	;;
+	(-u)
+	echo "Stopping Organizr to Update"
+	sleep 5
+	cd $Programloc
+	git pull
+	;;
+	(-U)
+	echo "Stopping Organizr to Force Update"
+	sleep 5
+	cd $Programloc
+	git fetch --all
+	git reset --hard origin/master
+	git pull
+	;;
+    	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0
