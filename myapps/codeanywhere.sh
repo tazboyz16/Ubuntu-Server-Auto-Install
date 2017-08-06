08619530b706c90514621dd62b2dffca4e77e225
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-06 18:17:48 -0400

    Update codeanywhere.sh

--- myapps/codeanywhere.sh
+++ myapps/codeanywhere.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-git checkout Dev
 #http://port-5050.ubuntu-server-auto-install-tazboyz16.codeanyapp.com/wizard/
 apt update; apt upgrade -y
 apt install systemd software-properties-common nano dialog debconf -y
