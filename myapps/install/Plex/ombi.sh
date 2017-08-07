d2e095668e4bd433bebc64f89d4a03e9d1f19630
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-07 17:36:24 -0400

    Update ombi.sh

--- myapps/install/Plex/ombi.sh
+++ myapps/install/Plex/ombi.sh
@@ -3,7 +3,7 @@
 echo "Installing PlexRequests.NET (Ombi)"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-apt get update; apt install mono-complete unzip -y
+apt update; apt install mono-complete unzip -y
 adduser --disabled-password --system --no-create-home --gecos "Ombi Service" --group ombi
 mkdir /opt/Ombi && cd /opt/Ombi
 wget $(curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep 'browser_' | cut -d\" -f4)
