fe2bfe34c8c72e84d3f736f67edef84cd888ec4d
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-12 19:58:04 -0400

    Update radarrinstall.sh

--- myapps/install/Radarr/radarrinstall.sh
+++ myapps/install/Radarr/radarrinstall.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
+versionm=$(lsb_release -cs)
 
+
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 apt update 
 apt install libmono-cil-dev curl mediainfo mono-devel -y
 adduser --disabled-password --system --home /opt/ProgramData/radarr --gecos "Radarr Service" --group radarr
-
 echo "<--- Downloading latest Radarr --->"
 cd /opt && wget https://github.com/Radarr/Radarr/releases/download/v0.2.0.778/Radarr.develop.0.2.0.778.linux.tar.gz
 tar -xvzf Radarr.develop.0.2.0.778.linux.tar.gz
-
-#cd /opt && sudo git clone https://github.com/Radarr/Radarr/releases
+rm -rf Radarr*.tar.gz
 chown -R radarr:radarr /opt/Radarr/
 chmod -R 0777 /opt/Radarr
-
 echo "Creating Startup Script"
 cp /opt/install/Radarr/radarr.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/radarr.service
