55ae1d312f544dffdd1211c0a2a17c9952af9584
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-12 19:48:41 -0400

    Update radarrinstall.sh

--- myapps/install/Radarr/radarrinstall.sh
+++ myapps/install/Radarr/radarrinstall.sh
@@ -3,7 +3,8 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 
-apt update && apt install libmono-cil-dev curl mediainfo mono-devel -y
+apt update 
+apt install libmono-cil-dev curl mediainfo mono-devel -y
 adduser --disabled-password --system --home /opt/ProgramData/radarr --gecos "Radarr Service" --group radarr
 
 echo "<--- Downloading latest Radarr --->"
@@ -18,4 +19,4 @@ echo "Creating Startup Script"
 cp /opt/install/Radarr/radarr.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/radarr.service
 systemctl enable radarr.service
-systemctl restart radarr.service 
+systemctl restart radarr.service
