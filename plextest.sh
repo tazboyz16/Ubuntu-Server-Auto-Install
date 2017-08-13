1194d6cf52f1cafd4b7da56473438f4147081735
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-12 22:55:45 -0400

    Delete plextest.sh

--- plextest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo deb https://downloads.plex.tv/repo/deb ./public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
-
-apt update
-apt install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes plexmediaserver
