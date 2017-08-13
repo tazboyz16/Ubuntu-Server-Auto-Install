75d2373109459c17f6b12ddf604c86a89ff327a7
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-12 20:05:18 -0400

    Update radarrinstall.sh

--- myapps/install/Radarr/radarrinstall.sh
+++ myapps/install/Radarr/radarrinstall.sh
@@ -13,7 +13,7 @@ echo "<--- Downloading latest Radarr --->"
 cd /opt && wget https://github.com/Radarr/Radarr/releases/download/v0.2.0.778/Radarr.develop.0.2.0.778.linux.tar.gz
 tar -xvzf Radarr.develop.0.2.0.778.linux.tar.gz
 sleep 5 #sleep for 5 sec to ensure all files are copied over
-rm -rf Radarr.develop.0.2.0.778.linux.tar.gz
+#rm -rf Radarr.develop.0.2.0.778.linux.tar.gz
 chown -R radarr:radarr /opt/Radarr/
 chmod -R 0777 /opt/Radarr
 echo "Creating Startup Script"
