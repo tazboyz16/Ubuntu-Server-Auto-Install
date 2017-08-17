6379449444b1ed1058b106df25fa41ddd0ba521e
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-16 23:19:41 -0400

    Update Webtools.sh

--- myapps/install/Plex/Webtools.sh
+++ myapps/install/Plex/Webtools.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+###########################################################
+# Created by @tazboyz16 
+# This Script was created at 
+# https://github.com/tazboyz16/Ubuntu-Server-Auto-Install
+# @ 2017 Creation
+# GNU General Public License v3.0
+###########################################################
+
+
 systemctl stop plexmediaserver
 sleep 15
 chmod 0777 -R /var/lib/plexmediaserver
