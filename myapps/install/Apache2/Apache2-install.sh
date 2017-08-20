946b179cae00289ce694be5a3a3fc36287ac221c
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-20 16:56:13 -0400

    Update Apache2-install.sh

--- myapps/install/Apache2/Apache2-install.sh
+++ myapps/install/Apache2/Apache2-install.sh
@@ -13,26 +13,25 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+
+
+
 apt update; apt install apache2 -y
 sleep 15
-
 echo "Enabling Mods to have Reverse Proxy to services"
 a2enmod rewrite proxy proxy_http headers
-
 echo "<--- Restoring Apache2 Settings --->"
 cat /opt/install/Apache2/apache2.conf > /etc/apache2/apache2.conf
-sleep 5
+cat /opt/install/Apache2/default-ssl.conf > /etc/apache2/sites-available/default-ssl.conf
+cat /opt/install/Apache2/000-default.conf > /etc/apache2/sites-available/000-default.conf
 echo "<- Restoring Apache2 Error Pages ->"
 cat /opt/install/Apache2/localized-error-pages.conf > /etc/apache2/conf-available/localized-error-pages.conf
 systemctl restart apache2
 
-echo "<--- Restoring WWW files for Website --->"
-rm -rf /var/www
-cd /opt/backup
-tar xjf /opt/install/Apache2/www.tar.bz2
-mv /opt/backup/www/ /var
-
-echo
-echo "<--- Changing Rights for dir WWW --->"
-chmod 0777 -R /var/www
-chown www-data:www-data -R /var/www
+#echo "<--- Restoring WWW files for Website --->"
+#rm -rf /var/www
+#cd /opt/backup
+#tar xjf /opt/install/Apache2/www.tar.bz2
+#mv /opt/backup/www/ /var
+#chmod 0777 -R /var/www
+#chown www-data:www-data -R /var/www
