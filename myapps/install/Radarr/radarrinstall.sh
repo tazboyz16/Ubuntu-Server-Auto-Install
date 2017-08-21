26f23e33e816984839d2c889ba8ffb8121a1e1fa
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-20 20:46:21 -0400

    Update radarrinstall.sh

--- myapps/install/Radarr/radarrinstall.sh
+++ myapps/install/Radarr/radarrinstall.sh
@@ -64,7 +64,8 @@ case $mode in
 	;;
 	(-t)
 	export VERSION=$(curl -s "https://api.github.com/repos/Radarr/Radarr/releases" | grep -o 'tag/[v.0-9]*' | awk -F/ '{print $2}')
-	curl -L https://github.com/OpenVPN/easy-rsa/releases/download/$VERSION/Radarr.develop-$VERSION.linux.tar.gz 
+	curl -L https://github.com/OpenVPN/easy-rsa/releases/download/$VERSION/Radarr.develop-$VERSION.linux.tar.gz
+	;;
     	(-*) echo "Invalid Argument"; exit 0;;
 esac
 exit 0
