bdd0e5ef9b99531dd0d105ecc3b5fec12bad2155
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-22 20:16:06 -0400

    Update windscribeCLI.sh

--- myapps/install/Windscribe/windscribeCLI.sh
+++ myapps/install/Windscribe/windscribeCLI.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
 echo 'deb https://repo.windscribe.com/ubuntu zesty main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
 apt update
