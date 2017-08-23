4233616c3b60124961c1d1d60df7e67c6d88869a
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-22 23:08:38 -0400

    Update sinusbot.sh

--- myapps/install/SinusBot/sinusbot.sh
+++ myapps/install/SinusBot/sinusbot.sh
@@ -13,35 +13,47 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-apt install debconf-utils lsb-release screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 less python -y
-adduser --disabled-password --system --home /opt/ProgramData/Sinusbot --gecos "Sinusbot Service" --group sinusbot
-mkdir -p /opt/Sinusbot
-cd /opt/Sinusbot
-wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
+#Modes (Variables)
+# b=backup i=install r=restore u=update U=Force Update 
+mode="$1"
 
-tar -xjf sinusbot-beta.tar.bz2
-#if new install
-cp config.ini.dist config.ini
-#Saved settings
-#cp /opt/install/SinusBot/config.ini /opt/Sinusbot/config.ini
+TeamSpeakClient=3.0.19.4
+Programloc=/opt/Sinusbot/
+backupdir=/opt/backup/Sinusbot
+time=$(date +"%m_%d_%y-%H_%M")
 
-chown -R sinusbot:sinusbot /opt/Sinusbot
-chmod 0777 -R /opt/Sinusbot
+case $mode in
+	(-i|"")
+	apt install debconf-utils lsb-release screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 less python -y
+	adduser --disabled-password --system --home /opt/ProgramData/Sinusbot --gecos "Sinusbot Service" --group sinusbot
+	mkdir -p $Programloc
+	cd $Programloc
+	wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
+	tar -xjf sinusbot-beta.tar.bz2
+	#if new install
+	cp config.ini.dist config.ini
+	chown -R sinusbot:sinusbot /opt/Sinusbot
+	chmod 0777 -R /opt/Sinusbot
+	wget http://dl.4players.de/ts/releases/$TeamSpeakClient/TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	chmod 0755 TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	printf '\ny\n' | LESS='+q' ./TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	wget https://yt-dl.org/downloads/latest/youtube-dl -o /opt/Sinusbot/youtube-dl
+	chmod a+rx /opt/Sinusbot/youtube-dl
+	cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
+	chown -R sinusbot:sinusbot /opt/Sinusbot
+	chmod 0777 -R /opt/Sinusbot
+	
+	echo "Creating Sinusbot startup Script"
+	cp /opt/install/SinusBot/sinusbot.service /etc/systemd/system/
+	chmod 644 /etc/systemd/system/sinusbot.service
+	systemctl enable sinusbot.service
+	systemctl restart sinusbot.service
+	;;
+    	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0
 
-wget http://dl.4players.de/ts/releases/3.0.18.2/TeamSpeak3-Client-linux_amd64-3.0.18.2.run
-chmod 0755 TeamSpeak3-Client-linux_amd64-3.0.18.2.run
-printf '\ny\n' | LESS='+q' ./TeamSpeak3-Client-linux_amd64-3.0.18.2.run
 
-wget https://yt-dl.org/downloads/latest/youtube-dl -o /opt/Sinusbot/youtube-dl
-chmod a+rx /opt/Sinusbot/youtube-dl
 
-cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
-
-chown -R sinusbot:sinusbot /opt/Sinusbot
-chmod 0777 -R /opt/Sinusbot
-
-echo "Creating Sinusbot startup Script"
-cp /opt/install/SinusBot/sinusbot.service /etc/systemd/system/
-chmod 644 /etc/systemd/system/sinusbot.service
-systemctl enable sinusbot.service
-systemctl restart sinusbot.service
+	#Saved settings
+	#cp /opt/install/SinusBot/config.ini /opt/Sinusbot/config.ini
