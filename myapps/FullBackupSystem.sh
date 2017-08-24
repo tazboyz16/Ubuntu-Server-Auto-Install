7a2160e37b629b58684b971fca29d10902fc6bb2
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-23 21:25:15 -0400

    Update FullBackupSystem.sh

--- myapps/FullBackupSystem.sh
+++ myapps/FullBackupSystem.sh
@@ -16,20 +16,20 @@ fi
 if [ -d /etc/apache2 ]; then
 bash /opt/install/Apache2/Apache2-install.sh -b
 else
-echo "Apache2 is not installed. No backup Require"
+echo "Apache2 is not installed. No Backup Require"
 fi
 echo
 
 if [ -d /opt/CouchPotato ]; then
 bash /opt/install/CouchPotato/couchpotato-installer.sh -b
 else
-echo "CouchPotato is not installed. No backup Require"
+echo "CouchPotato is not installed. No Backup Require"
 fi
 echo
 
 if [ -d /var/lib/deluge ]; then
 bash /opt/install/Deluge/deluge_webui.sh -b
 else
-echo "Deluge is not installed. No backup Require"
+echo "Deluge is not installed. No Backup Require"
 fi
 echo
