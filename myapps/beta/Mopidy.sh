1396375f7f8774b219576983124bc3896452765b
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-23 23:50:51 -0400

    Update Mopidy.sh

--- myapps/beta/Mopidy.sh
+++ myapps/beta/Mopidy.sh
@@ -12,13 +12,20 @@ if [[ $EUID -ne 0 ]]; then
 	echo "This Script must be run as root"
 	exit 1
 fi
+
+
+
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
-apt update; apt install mopidy -y
+apt update; apt install mopidy python-pip -y
 
 #~/.config/mopidy/mopidy.conf
 #mopidy config to effective configuration
 #https://docs.mopidy.com/en/latest/service/#service
+#have to edit http section to enable outside localhost connections
 
-pip install Mopidy-Moped
 # http://localhost:6680/moped
+pip install Mopidy-Moped
+#
+pip install mpoidy-mopfiy
+
