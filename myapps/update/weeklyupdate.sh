28e8a1dd42ba53646d1035d10264451443854652
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-27 23:02:17 -0400

    Update weeklyupdate.sh

--- myapps/update/weeklyupdate.sh
+++ myapps/update/weeklyupdate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+###########################################################
+# Created by @tazboyz16 
+# This Script was created at 
+# https://github.com/tazboyz16/Ubuntu-Server-Auto-Install
+# @ 2017 Copyright
+# GNU General Public License v3.0
+###########################################################
+
 echo "Updating all Git Repos"
 echo
 
