d94fee4b6251a08f60de1c18e2d891aef12a4f2b
tazboyz16 <tazboyz_16@yahoo.com>
2017-08-30 22:03:52 -0400

    Update sinusbot.sh

--- myapps/install/SinusBot/sinusbot.sh
+++ myapps/install/SinusBot/sinusbot.sh
@@ -95,7 +95,7 @@ case $mode in
 	;;
 	(-passwd)
 	echo "Reseting Sinusbot Service back to normal operations"
- 	sytemctl stop sinusbot
+ 	systemctl stop sinusbot
 	sed -i 's/ -pwreset=foobar/ /g' /etc/systemd/system/sinusbot.service
 	systemctl daemon-reload
 	systemctl restart sinusbot
