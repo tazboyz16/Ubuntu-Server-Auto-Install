7cee6884bd5630d4ab499c831523a1de642865e3
tazboyz16 <tazboyz_16@yahoo.com>
2017-09-02 08:34:59 -0400

    dev

--- myapps/install/Glances/Glances.sh
+++ myapps/install/Glances/Glances.sh
@@ -3,5 +3,5 @@
 
 
 #install Glances
-apt install python-pip python-psutil -y
+apt install python-pip -y
 pip install bernhard bottle cassandra-driver couchdb docker elasticsearch hddtemp influxdb kafka-python matplotlib netifaces nvidia-ml-py3 pika potsdb prometheus_client py-cpuinfo pymdstat pysnmp pystache pyzmq requests scandir statsd wifi zeroconf
