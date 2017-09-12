eac308468a307a137b6a94689cddf3c624fa75e4
tazboyz16 <tazboyz_16@yahoo.com>
2017-09-12 16:54:53 -0400

    adding backup and restore options plus name fix

--- myapps/install/Mopidy/Mopidyinstall.sh
+++ myapps/install/Mopidy/Mopidyinstall.sh
@@ -13,25 +13,36 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-
-
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
-apt update; apt install mopidy python-pip -y
-
 #~/.config/mopidy/mopidy.conf
 #/etc/mopidy/mopidy.conf
 #mopidy config to effective configuration
 #https://docs.mopidy.com/en/latest/service/#service
 #have to edit http section to enable outside localhost connections
+# http://localhost:6680/moped & http://localhost:6680/mopify
 
-# http://localhost:6680/moped
-pip install mopidy-Moped
-# http://localhost:6680/mopify
-pip install mopidy-mopfiy
+#Modes (Variables)
+# b=(backup) i=(install) r=(restore) u=(update) U=(Force Update) proxy=(Reverse Proxy) port=(Change port)
+mode="$1"
+Programloc=/opt/Mopidy
+backupdir=/opt/backup/Mopidy
 
-echo "Creating Startup Script"
-cp /opt/install/Mopidy/mopidy.service /etc/systemd/system/
-chmod 644 /etc/systemd/system/mopidy.service
-systemctl enable mopidy.service
-systemctl restart mopidy.service 
+case $mode in
+	(-i|"")
+	wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+	wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
+	apt update; apt install mopidy python-pip -y
+	pip install Mopidy-Moped
+	pip install Mopidy-Mopify
+	echo "Creating Startup Script"
+	cp /opt/install/Mopidy/mopidy.service /etc/systemd/system/
+	chmod 644 /etc/systemd/system/mopidy.service
+	systemctl enable mopidy.service
+	systemctl restart mopidy.service 
+	;;
+	(-r)
+	;;
+	(-b)
+	;;
+	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0	
