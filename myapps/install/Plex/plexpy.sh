c2e1d2f62d06e4eda4a9a39caff10410661f9080
tazboyz16 <tazboyz_16@yahoo.com>
2017-09-13 17:35:13 -0400

    Update plexpy.sh

--- myapps/install/Plex/plexpy.sh
+++ myapps/install/Plex/plexpy.sh
@@ -13,17 +13,54 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-echo "Installing PlexPy"
-apt update
-apt install python -y
-adduser --disabled-password --system --home /opt/ProgramData/PlexPy --gecos "PlexPy Service"  --group plexpy
-cd /opt && git clone https://github.com/JonnyWong16/plexpy.git /opt/Plexpy
-#echo "<--- Restoring PLexPy Settings --->"
-#cat /opt/install/PlexAddons/Headphones.txt > /opt/.headphones/config.ini
-chown -R plexpy:plexpy /opt/Plexpy
-chmod -R 0777 /opt/Plexpy
-echo "Creating Startup Script for PlexPy"
-cp /opt/install/Plex/plexpy.service /etc/systemd/system/
-chmod 0777 /etc/systemd/system/plexpy.service
-systemctl enable plexpy.service
-systemctl restart plexpy.service
+#Modes (Variables)
+# b=backup i=install r=restore 
+mode="$1"
+Programloc=/opt/Plexpy
+backupdir=/opt/backup/Plexpy
+
+case $mode in
+	(-i|"")
+	echo "Installing PlexPy"
+	apt update
+	apt install python -y
+	adduser --disabled-password --system --home /opt/ProgramData/PlexPy --gecos "PlexPy Service"  --group plexpy
+	cd /opt && git clone https://github.com/JonnyWong16/plexpy.git /opt/Plexpy
+	#echo "<--- Restoring PLexPy Settings --->"
+	#cat /opt/install/PlexAddons/Headphones.txt > /opt/.headphones/config.ini
+	chown -R plexpy:plexpy /opt/Plexpy
+	chmod -R 0777 /opt/Plexpy
+	echo "Creating Startup Script for PlexPy"
+	cp /opt/install/Plex/plexpy.service /etc/systemd/system/
+	chmod 0777 /etc/systemd/system/plexpy.service
+	systemctl enable plexpy.service
+	systemctl restart plexpy.service
+	;;
+	(-r)
+	echo "<--Restoring Plexpy Settings -->"
+	echo "Stopping Plexpy"
+	systemctl stop plexpy
+	cd /opt/backup
+	tar -xvzf /opt/backup/Plexpy_Backup.tar.gz
+	cp -rf plexpy.db /opt/Plexpy/; rm -rf  plexpy.db
+	cp -rf config.ini /opt/Plexpy/; rm -rf config.ini
+	echo "Restarting up Plexpy"
+	systemctl start plexpy
+	;;
+	(-b)
+	echo "Stopping Plexpy"
+    	systemctl stop plexpy
+    	echo "Making sure Backup Dir exists"
+    	mkdir -p $backupdir
+    	echo "Backing up Plexpy to /opt/backup"
+	cp /opt/Plexpy/plexpy.db $backupdir
+	cp /opt/Plexpy/config.ini $backupdir
+	cd $backupdir
+	tar -zcvf /opt/backup/Plexpy_Backup.tar.gz *
+	rm -rf $backupdir
+    	echo "Restarting up Plexpy"
+	systemctl start plexpy
+	;;
+    	(-*) echo "Invalid Argument"; exit 0;;
+esac
+exit 0
