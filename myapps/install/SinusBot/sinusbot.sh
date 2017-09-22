921a39e654630296d68839d2716cf141874eb5e1
tazboyz16 <tazboyz_16@yahoo.com>
2017-09-22 12:48:27 -0400

    Update sinusbot.sh

--- myapps/install/SinusBot/sinusbot.sh
+++ myapps/install/SinusBot/sinusbot.sh
@@ -14,21 +14,19 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #Modes (Variables)
-# b=backup i=install r=restore u=update(coming soon) passwd=(Remove the Forced Foobarr password)
+# b=backup i=install r=restore u=update passwd=(Remove the Forced Foobarr password)
 mode="$1"
 
 TeamSpeakClient=3.0.19.4
 Programloc=/opt/Sinusbot
 backupdir=/opt/backup/Sinusbot
-time=$(date +"%m_%d_%y-%H_%M")
 
 case $mode in
 	(-i|"")
 	apt install debconf-utils lsb-release screen x11vnc xvfb libxcursor1 ca-certificates bzip2 psmisc libglib2.0-0 less python -y
 	adduser --disabled-password --system --home /opt/ProgramData/Sinusbot --gecos "Sinusbot Service" --group sinusbot
-	mkdir -p $Programloc
-	cd $Programloc
-	wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
+	wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2 -P $Programloc/
+	cd $Programloc/
 	tar -xjf sinusbot-beta.tar.bz2
 	rm sinusbot-beta.tar.bz2
 	#if new install
@@ -39,7 +37,7 @@ case $mode in
 	chmod 0755 TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
 	printf '\ny\n' | LESS='+q' ./TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
 	rm TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
-	wget https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+	wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 	chmod a+rx /usr/local/bin/youtube-dl
 	cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
 	chown -R sinusbot:sinusbot $Programloc
@@ -60,22 +58,29 @@ case $mode in
 	echo "<--- Restoring Sinusbot Settings --->"
 	echo "Stopping Sinusbot"
 	systemctl stop sinusbot
-	cat /opt/install/sinusbot/v.txt > /opt/HTPCManager/userdata
-	chown -R sinusbot:sinusbot /opt/sinusbot
-	chmod -R 0777 /opt/sinusbot
+	cd /opt/backup
+	tar -xvzf /opt/backup/Sinusbot_Backup.tar.gz
+	cp -rf config.ini $Programloc; rm -rf config.ini
+	cp -rf data/ $Programloc; rm -rf data/
+	chown -R sinusbot:sinusbot $Programloc
+	chmod -R 0777 $Programloc
 	echo "Starting up Sinusbot"
 	systemctl start sinusbot
 	;;
 	(-b)
 	#Saved settings
 	#cp /opt/install/SinusBot/config.ini /opt/Sinusbot/config.ini
+	#and cp /data folder
 	echo "Stopping Sinusbot"
     	systemctl stop sinusbot
     	echo "Making sure Backup Dir exists"
     	mkdir -p $backupdir
     	echo "Backing up Sinusbot to /opt/backup"
-	cp -rf /opt/Sinusbot/userdata $backupdir
-    	tar -zcvf /opt/backup/Sinusbot_FullBackup-$time.tar.gz $backupdir
+	cp -rf /opt/Sinusbot/config.ini $backupdir
+	cp -rf /opt/Sinusbot/data $backupdir
+	cd $backupdir
+    	tar -zcvf /opt/backup/Sinusbot_Backup.tar.gz *
+	rm -rf $backupdir
     	echo "Restarting up Sinusbot"
 	systemctl start sinusbot
 	;;
@@ -85,8 +90,27 @@ case $mode in
 		echo "Sinusbot not installed at '$Programloc'. Update Failed"
 		exit 0;
 		fi
+	echo "Performing Backup of SinusBot Dir"
 	bash /opt/install/SinusBot/sinusbot.sh -b
-	bash /opt/install/SinusBot/sinusbot.sh -i
+	
+	echo "Updating SinusBot"
+	systemctl stop sinusbot
+	cd $Programloc
+	wget https://www.sinusbot.com/dl/sinusbot-beta.tar.bz2
+	tar -xjf sinusbot-beta.tar.bz2
+	rm sinusbot-beta.tar.bz2
+	chown -R sinusbot:sinusbot $Programloc
+	chmod 0777 -R $Programloc
+	wget http://dl.4players.de/ts/releases/$TeamSpeakClient/TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	chmod 0755 TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	printf '\ny\n' | LESS='+q' ./TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	rm TeamSpeak3-Client-linux_amd64-$TeamSpeakClient.run
+	wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+	chmod a+rx /usr/local/bin/youtube-dl
+	cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
+	chown -R sinusbot:sinusbot $Programloc
+	chmod 0777 -R $Programloc
+	systemctl restart sinusbot.service
 	;;
 	(-passwd)
 	echo "Reseting Sinusbot Service back to normal operations"
