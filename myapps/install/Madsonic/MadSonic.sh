0c9b5c27fece94f91520e5a59e2986e1d7cc3822
tazboyz16 <tazboyz_16@yahoo.com>
2017-09-30 23:04:29 -0400

    Update MadSonic.sh

--- myapps/install/Madsonic/MadSonic.sh
+++ myapps/install/Madsonic/MadSonic.sh
@@ -25,33 +25,14 @@ Programloc=/etc/default/madsonic
 backupdir=/opt/backup/Madsonic
 
 #grabing latest version number and its download page
-mainversion=$(curl http://beta.madsonic.org/pages/download.jsp | grep -P -e "stable" | grep -Po -e "[\d]{1,3}.[\d]{1,3}.[\d]{1,6}" | cut -c1-3 | tr -d '.')
-stableversion=$(curl http://beta.madsonic.org/pages/download.jsp | grep -P -e "stable" | grep -Po -e "[\d]{1,3}.[\d]{1,3}.[\d]{1,6}")
 #http://beta.madsonic.org/pages/download62.jsp
-
-
-
-
-
-
 version=$(curl http://beta.madsonic.org/pages/download.jsp | grep -P -e "stable" | grep -Po -e "[\d]{1,3}.[\d]{1,3}.[\d]{1,6}" | cut -c1-3 | tr -d '.')
-echo $version
 mainversion=$(curl http://beta.madsonic.org/pages/download.jsp | grep -P -e "stable" | grep -Po -e "[\d]{1,3}.[\d]{1,3}.[\d]{1,6}" | cut -c1-3)
-echo $mainversion
 stableversion=$(curl http://beta.madsonic.org/pages/download.jsp | grep -P -e "stable" | grep -Po -e "[\d]{1,3}.[\d]{1,3}.[\d]{1,6}")
-echo $stableversion
-
 dateversion=$(curl http://beta.madsonic.org/pages/download$version.jsp | grep -Pe "[\d]+_madsonic-6.2.9040.deb" | grep -Poe "request.jsp\?branch=6.2&target=[\d]+_madsonic-6.2.9040.deb" | grep -Poe "[\d]{8}")
-echo $dateversion
-
-wget http://madsonic.org/download/$mainversion/$dateversion\_madsonic-$stableversion.deb -O madsonic.deb
 
 #madsonic.org/download/6.2/20161208_madsonic-6.2.9040.deb
 
-
-
-beta.madsonic.org/pages/
-wget http://beta.madsonic.org/pages/request.jsp?branch=$mainversion&target=*_madsonic-$stableversion.deb
 case $mode in
 	(-i|"")
 	add-apt-repository -y ppa:webupd8team/java
@@ -60,9 +41,9 @@ case $mode in
 	echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 	apt install oracle-java8-installer -y
 	adduser --disabled-password --system --home /opt/ProgramData/Madsonic --gecos "Madsonic Service" --group Madsonic
-	wget http://madsonic.org/download/$miniversion/$MadSonicDeb
-	dpkg -i $MadSonicDeb
-	rm -rf $MadSonicDeb
+	wget http://madsonic.org/download/$mainversion/$dateversion\_madsonic-$stableversion.deb -O madsonic.deb
+	dpkg -i madsonic.deb
+	rm -rf madsonic.deb
 	chmod 0777 -R /var/madsonic
 	#Changing user account choice vs running as root
 	service madsonic stop
