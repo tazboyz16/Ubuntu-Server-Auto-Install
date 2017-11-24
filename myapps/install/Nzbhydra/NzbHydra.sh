3025670179f834bcd5adb8fe3a7d8b83cc000360
tazboyz16 <tazboyz_16@yahoo.com>
2017-11-23 21:46:12 -0500

    Update and rename myapps/beta/NzbHydra.sh to myapps/install/Nzbhydra/NzbHydra.sh

--- myapps/install/Nzbhydra/NzbHydra.sh
+++ myapps/install/Nzbhydra/NzbHydra.sh
@@ -15,8 +15,13 @@ fi
 
 
 adduser --disabled-password --system --home /opt/ProgramData/Nzbhydra --gecos "Ubooquity Service" --group Nzbhydra
-apt update; apt install python 2.7 git-core -y
-add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
+apt update; apt install python2.7 git-core -y
 apt update; apt upgrade -y
 git clone https://github.com/theotherp/nzbhydra /opt/Nzbhydra
 sudo chown -R Nzbhydra:Nzbhydra /opt/Nzbhydra
+
+echo "Creating Startup Script"
+cp /opt/install/Nzbhydra/nzbhydra.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/nzbhydra.service
+systemctl enable nzbhydra.service
+systemctl restart nzbhydra.service 
