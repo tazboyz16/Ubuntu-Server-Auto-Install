7b4f1682df2e081f8cdeb15bf7b115c1ae80607a
tazboyz16 <tazboyz_16@yahoo.com>
2017-12-04 15:27:23 -0500

    Update headphones-installer.sh

--- myapps/install/Headphones/headphones-installer.sh
+++ myapps/install/Headphones/headphones-installer.sh
@@ -38,6 +38,11 @@ case $mode in
 	chmod 644 /etc/systemd/system/headphones.service
 	systemctl enable headphones.service
 	systemctl restart headphones.service
+	#Checking if Iptables is installed and updating with port settings
+	    if [ -f /etc/default/iptables ]; then
+	        sed -i "s/#-A INPUT -p tcp --dport 8182 -j ACCEPT/-A INPUT -p tcp --dport 8182 -j ACCEPT/g" /etc/default/iptables
+	        /etc/init.d/iptables restart
+        fi
 	;;
 	(-r)
 	echo "<--- Restoring Headphones Settings --->"
