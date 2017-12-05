fb2f3fabd0e0fb8cf5f34b721dea8d75dc939f04
tazboyz16 <tazboyz_16@yahoo.com>
2017-12-04 22:59:56 -0500

    Update NzbHydra.sh

--- myapps/install/Nzbhydra/NzbHydra.sh
+++ myapps/install/Nzbhydra/NzbHydra.sh
@@ -25,3 +25,9 @@ cp /opt/install/Nzbhydra/nzbhydra.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/nzbhydra.service
 systemctl enable nzbhydra.service
 systemctl restart nzbhydra.service 
+
+#Checking if Iptables is installed and updating with port settings
+	    if [ -f /etc/default/iptables ]; then
+	        sed -i "s/#-A INPUT -p tcp --dport 5075 -j ACCEPT/-A INPUT -p tcp --dport 5075 -j ACCEPT/g" /etc/default/iptables
+	        /etc/init.d/iptables restart
+	   fi
