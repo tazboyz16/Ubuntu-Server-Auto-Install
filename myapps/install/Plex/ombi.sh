d8d2ee00ec687c4334dafad217a2f5caf1772aa7
tazboyz16 <tazboyz_16@yahoo.com>
2017-12-10 11:07:59 -0500

    Update ombi.sh

--- myapps/install/Plex/ombi.sh
+++ myapps/install/Plex/ombi.sh
@@ -26,8 +26,10 @@ backupdir=/opt/backup/Ombi
 case $mode in
 	(-i|"")
 	echo "Installing PlexRequests.NET (Ombi)"
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+		if [ ! -f /etc/apt/sources.list.d/mono-xamarin.list ]; then
+		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+		echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+		fi
 	apt update; apt install mono-complete unzip -y
 	adduser --disabled-password --system --home /opt/ProgramData/Ombi --gecos "Ombi Service" --group ombi
 	mkdir /opt/Ombi && cd $Programloc
