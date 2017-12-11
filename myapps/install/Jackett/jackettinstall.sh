d5156932bbdaf1873a9e93ed4baba50df5e19b33
tazboyz16 <tazboyz_16@yahoo.com>
2017-12-10 19:51:17 -0500

    Update jackettinstall.sh

--- myapps/install/Jackett/jackettinstall.sh
+++ myapps/install/Jackett/jackettinstall.sh
@@ -24,10 +24,8 @@ backupdir=/opt/backup/Jackett
 case $mode in
 	(-i|"")
 	adduser --disabled-password --system --home /opt/ProgramData/Jackett --gecos "Jackett Service" --group Jackett
-		if [ ! -f /etc/apt/sources.list.d/mono-xamarin.list ]; then
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-		echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-		fi
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 	apt update
 	apt install mono-complete libcurl4-openssl-dev -y
 	wget $(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | grep 'Jackett.Binaries.Mono.tar.gz' | cut -d\" -f4)
