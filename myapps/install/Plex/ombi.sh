15c961d318b6f3f726db4c73c94e5daae6c36936
tazboyz16 <tazboyz_16@yahoo.com>
2017-12-13 23:06:46 -0500

    Update ombi.sh

--- myapps/install/Plex/ombi.sh
+++ myapps/install/Plex/ombi.sh
@@ -25,7 +25,6 @@ backupdir=/opt/backup/Ombi
 
 case $mode in
 	(-i|"")
-	echo "Installing PlexRequests.NET (Ombi)"
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb http://download.mono-project.com/repo/ubuntu $versionm main" | sudo tee /etc/apt/sources.list.d/mono-offical.list
 	apt update; apt install mono-complete unzip -y
